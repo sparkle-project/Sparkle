904f68e3e3dcf3ece264ae759652c1257fcc0b65
andym <andym@localhost>
2008-01-26 14:57:33 +0000

    Added beginnings of permission prompt.

---
+++ SUUpdatePermissionPrompt.h
@@ -0,0 +1,20 @@
+//
+//  SUUpdatePermissionPrompt.h
+//  Sparkle
+//
+//  Created by Andy Matuschak on 1/24/08.
+//  Copyright 2008 Andy Matuschak. All rights reserved.
+//
+
+#ifndef SUUPDATEPERMISSIONPROMPT_H
+#define SUUPDATEPERMISSIONPROMPT_H
+
+#import "Sparkle.h"
+
+@interface SUUpdatePermissionPrompt : NSWindowController {
+
+}
+
+@end
+
+#endif
---
+++ SUUpdatePermissionPrompt.m
@@ -0,0 +1,14 @@
+//
+//  SUUpdatePermissionPrompt.m
+//  Sparkle
+//
+//  Created by Andy Matuschak on 1/24/08.
+//  Copyright 2008 Andy Matuschak. All rights reserved.
+//
+
+#import "SUUpdatePermissionPrompt.h"
+
+
+@implementation SUUpdatePermissionPrompt
+
+@end
