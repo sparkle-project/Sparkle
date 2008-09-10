ccd44c0cb28ac1d6df445feae3f23bb78f55778d
Andy Matuschak <andy@andymatuschak.org>
2008-09-10 17:48:57 -0500

    Fixes 268723
    Now forcing a DSA signature check if the client provides a public key with the app.

--- SUBasicUpdateDriver.m
+++ SUBasicUpdateDriver.m
@@ -160,7 +160,7 @@
 {
 	// New in Sparkle 1.5: we're now checking signatures on all non-secure downloads, where "secure" is defined as both the appcast and the download being transmitted over SSL.
 	NSURL *downloadURL = [[d request] URL];
-	if (!([[downloadURL scheme] isEqualToString:@"https"] && [[appcastURL scheme] isEqualToString:@"https"]))
+	if (![[downloadURL scheme] isEqualToString:@"https"] || ![[appcastURL scheme] isEqualToString:@"https"] || [host publicDSAKey])
 	{
 		if (![SUDSAVerifier validatePath:downloadPath withEncodedDSASignature:[updateItem DSASignature] withPublicDSAKey:[host publicDSAKey]])
 		{
