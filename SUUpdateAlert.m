8ff6c147f26600c9dc15d6f76bed5730dda68af6
Andy Matuschak <andy@andymatuschak.org>
2008-12-27 23:14:55 -0600

    Fixing 301644: Limit WebView Contextual Menu
    Thanks to David Dauer for the patch.

--- SUUpdateAlert.m
+++ SUUpdateAlert.m
@@ -191,6 +191,38 @@
     }
 }
 
+// Clean up the contextual menu.
+- (NSArray *)webView:(WebView *)sender contextMenuItemsForElement:(NSDictionary *)element defaultMenuItems:(NSArray *)defaultMenuItems
+{
+	NSMutableArray *webViewMenuItems = [[defaultMenuItems mutableCopy] autorelease];
+	
+	if (webViewMenuItems)
+	{
+		NSEnumerator *itemEnumerator = [defaultMenuItems objectEnumerator];
+		NSMenuItem *menuItem = nil;
+		while ((menuItem = [itemEnumerator nextObject]))
+		{
+			NSInteger tag = [menuItem tag];
+			
+			switch (tag)
+			{
+				case WebMenuItemTagOpenLinkInNewWindow:
+				case WebMenuItemTagDownloadLinkToDisk:
+				case WebMenuItemTagOpenImageInNewWindow:
+				case WebMenuItemTagDownloadImageToDisk:
+				case WebMenuItemTagOpenFrameInNewWindow:
+				case WebMenuItemTagGoBack:
+				case WebMenuItemTagGoForward:
+				case WebMenuItemTagStop:
+				case WebMenuItemTagReload:		
+					[webViewMenuItems removeObjectIdenticalTo: menuItem];
+			}
+		}
+	}
+	
+	return webViewMenuItems;
+}
+
 - (void)setDelegate:del
 {
 	delegate = del;
