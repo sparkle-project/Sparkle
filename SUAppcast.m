bd80144c7b67b7a66c215e40174dfb9310843573
Andy Matuschak <andy@andymatuschak.org>
2009-10-20 13:55:03 -0700

    Fixing #456514: Appcast download crashes on 10.5 if download fails

--- SUAppcast.m
+++ SUAppcast.m
@@ -180,11 +180,14 @@
 - (void)download:(NSURLDownload *)download didFailWithError:(NSError *)error
 {
 	CFRelease(download);
+	if (downloadFilename)
+	{
 #if MAC_OS_X_VERSION_MIN_REQUIRED <= MAC_OS_X_VERSION_10_4
-    [[NSFileManager defaultManager] removeFileAtPath:downloadFilename handler:nil];
+		[[NSFileManager defaultManager] removeFileAtPath:downloadFilename handler:nil];
 #else
-    [[NSFileManager defaultManager] removeItemAtPath:downloadFilename error:NULL];
+		[[NSFileManager defaultManager] removeItemAtPath:downloadFilename error:NULL];
 #endif
+	}
     [downloadFilename release];
     downloadFilename = nil;
     
