2e0f0211d1923884a0501e8959f2bdd8e6c9ba00
Uli Kusterer <ulivc@zathras.de>
2009-12-08 00:37:39 +0100

    We now really put the old app in the trash (and any file we have to move out of the way to install a new app when we reset an edited name), even on non-admin user accounts. +++ TODO: Work around double authorization.

--- SUPlainInstallerInternals.m
+++ SUPlainInstallerInternals.m
@@ -125,6 +125,7 @@ static BOOL AuthorizationExecuteWithPrivilegesAndWait(AuthorizationRef authoriza
 		tempDir = [path stringByDeletingLastPathComponent];
 	
 	// Let's try to read the version number so the filename will be more meaningful.
+	#if TRY_TO_APPEND_VERSION_NUMBER
 	NSString *postFix = nil;
 	NSString *version = nil;
 	if ((version = [[NSBundle bundleWithPath: path] objectForInfoDictionaryKey:@"CFBundleVersion"]) && ![version isEqualToString:@""])
@@ -138,6 +139,9 @@ static BOOL AuthorizationExecuteWithPrivilegesAndWait(AuthorizationRef authoriza
 	else
 		postFix = @"old";
 	NSString *prefix = [NSString stringWithFormat: @"%@ (%@)", [[path lastPathComponent] stringByDeletingPathExtension], postFix];
+	#else
+	NSString *prefix = [[path lastPathComponent] stringByDeletingPathExtension];
+	#endif
 	NSString *tempName = [prefix stringByAppendingPathExtension: [path pathExtension]];
 	tempDir = [tempDir stringByAppendingPathComponent: tempName];
 	
@@ -153,8 +157,6 @@ static BOOL AuthorizationExecuteWithPrivilegesAndWait(AuthorizationRef authoriza
 {
 	// *** MUST BE SAFE TO CALL ON NON-MAIN THREAD!
 	
-	//BOOL		foundTrash = NO;	// +++ Using trash as tmp folder to begin with would be way cooler, IMHO.
-	//NSString*	tmp = [self _temporaryCopyNameForPath:dst didFindTrash: &foundTrash];
 	const char* srcPath = [src fileSystemRepresentation];
 	const char* tmpPath = [tmp fileSystemRepresentation];
 	const char* dstPath = [dst fileSystemRepresentation];
@@ -204,11 +206,11 @@ static BOOL AuthorizationExecuteWithPrivilegesAndWait(AuthorizationRef authoriza
 			res = AuthorizationExecuteWithPrivilegesAndWait( auth, "/bin/mv", kAuthorizationFlagDefaults, mvParams2 );
 		}
 		
-		if( res && haveDst /*&& !foundTrash*/ )	// If we managed to put the old exe in the trash, leave it there for the user to delete or recover.
-		{									// ...  Otherwise we better delete it, wouldn't want dozens of old versions lying around next to the new one.
-			const char* rmParams2[] = { "-rf", tmpPath, NULL };
-			res = AuthorizationExecuteWithPrivilegesAndWait( auth, "/bin/rm", kAuthorizationFlagDefaults, rmParams2 );
-		}
+//		if( res && haveDst /*&& !foundTrash*/ )	// If we managed to put the old exe in the trash, leave it there for the user to delete or recover.
+//		{									// ...  Otherwise we better delete it, wouldn't want dozens of old versions lying around next to the new one.
+//			const char* rmParams2[] = { "-rf", tmpPath, NULL };
+//			res = AuthorizationExecuteWithPrivilegesAndWait( auth, "/bin/rm", kAuthorizationFlagDefaults, rmParams2 );
+//		}
 		
 		// If the currently-running application is trusted, the new
 		// version should be trusted as well.  Remove it from the
@@ -244,19 +246,105 @@ static BOOL AuthorizationExecuteWithPrivilegesAndWait(AuthorizationRef authoriza
 	return res;
 }
 
++ (BOOL)_movePathWithForcedAuthentication:(NSString *)src toPath:(NSString *)dst error:(NSError **)error
+{
+	// *** MUST BE SAFE TO CALL ON NON-MAIN THREAD!
+	
+	const char* srcPath = [src fileSystemRepresentation];
+	const char* dstPath = [dst fileSystemRepresentation];
+	const char* dstContainerPath = [[dst stringByDeletingLastPathComponent] fileSystemRepresentation];
+	
+	struct stat dstSB;
+	stat(dstContainerPath, &dstSB);
+	
+	AuthorizationRef auth = NULL;
+	OSStatus authStat = errAuthorizationDenied;
+	while (authStat == errAuthorizationDenied) {
+		authStat = AuthorizationCreate(NULL,
+									   kAuthorizationEmptyEnvironment,
+									   kAuthorizationFlagDefaults,
+									   &auth);
+	}
+	
+	BOOL res = NO;
+	if (authStat == errAuthorizationSuccess)
+	{
+		res = YES;
+		
+		char uidgid[42];
+		snprintf(uidgid, sizeof(uidgid), "%d:%d",
+				 dstSB.st_uid, dstSB.st_gid);
+		
+		if( res )	// Set permissions while it's still in source, so we have it with working and correct perms when it arrives at destination.
+		{
+			const char* coParams[] = { "-R", uidgid, srcPath, NULL };
+			res = AuthorizationExecuteWithPrivilegesAndWait( auth, "/usr/sbin/chown", kAuthorizationFlagDefaults, coParams );
+			if( !res )
+				SULog(@"Can't set permissions");
+		}
+		
+		BOOL	haveDst = [[NSFileManager defaultManager] fileExistsAtPath: dst];
+		if( res && haveDst )	// If there's something at our tmp path (previous failed update or whatever) delete that first.
+		{
+			const char*	rmParams[] = { "-rf", dstPath, NULL };
+			res = AuthorizationExecuteWithPrivilegesAndWait( auth, "/bin/rm", kAuthorizationFlagDefaults, rmParams );
+			if( !res )
+				SULog(@"Can't remove destination file");
+		}
+		
+		if( res )	// Move!.
+		{
+			const char* mvParams[] = { "-f", srcPath, dstPath, NULL };
+			res = AuthorizationExecuteWithPrivilegesAndWait( auth, "/bin/mv", kAuthorizationFlagDefaults, mvParams );
+			if( !res )
+				SULog(@"Can't move source file");
+		}
+		
+		AuthorizationFree(auth, 0);
+		
+		if (!res)
+		{
+			// Something went wrong somewhere along the way, but we're not sure exactly where.
+			NSString *errorMessage = [NSString stringWithFormat:@"Authenticated file move from %@ to %@ failed.", src, dst];
+			if (error != NULL)
+				*error = [NSError errorWithDomain:SUSparkleErrorDomain code:SUAuthenticationFailure userInfo:[NSDictionary dictionaryWithObject:errorMessage forKey:NSLocalizedDescriptionKey]];
+		}
+	}
+	else
+	{
+		if (error != NULL)
+			*error = [NSError errorWithDomain:SUSparkleErrorDomain code:SUAuthenticationFailure userInfo:[NSDictionary dictionaryWithObject:@"Couldn't get permission to authenticate." forKey:NSLocalizedDescriptionKey]];
+	}
+	return res;
+}
+
 + (void)_movePathToTrash:(NSString *)path
 {
+	//SULog(@"Moving %@ to the trash.", path);
 	NSInteger tag = 0;
 	if (![[NSWorkspace sharedWorkspace] performFileOperation:NSWorkspaceRecycleOperation source:[path stringByDeletingLastPathComponent] destination:@"" files:[NSArray arrayWithObject:[path lastPathComponent]] tag:&tag])
-		NSLog(@"Sparkle error: couldn't move %@ to the trash. This is often a sign of a permissions error.", path);
+	{
+		BOOL		didFindTrash = NO;
+		NSString*	trashPath = [self _temporaryCopyNameForPath: path didFindTrash: &didFindTrash];
+		if( didFindTrash )
+		{
+			NSError		*err = nil;
+			if( ![self _movePathWithForcedAuthentication: path toPath: trashPath error: &err] )
+				SULog(@"Sparkle error: couldn't move %@ to the trash (%@). %@", path, trashPath, err);
+		}
+		else
+			SULog(@"Sparkle error: couldn't move %@ to the trash. This is often a sign of a permissions error.", path);
+	}
+	else
+		;//SULog(@"Moved %@ to the trash.", path);
 }
 
 + (BOOL)copyPathWithAuthentication:(NSString *)src overPath:(NSString *)dst temporaryName:(NSString *)tmp error:(NSError **)error
 {
-	FSRef		srcRef, dstRef, targetRef, movedRef;
+	FSRef		srcRef, dstRef, dstDirRev, movedRef, tmpDirRef;
 	OSErr		err;
-	BOOL		hadFileAtDest = NO;
-	NSString	*tmpPath = [[dst stringByDeletingLastPathComponent] stringByAppendingPathComponent:tmp];
+	BOOL		hadFileAtDest = NO, didFindTrash = NO;
+	NSString	*tmpPath = [self _temporaryCopyNameForPath: dst didFindTrash: &didFindTrash];
 	
 	err = FSPathMakeRefWithOptions((UInt8 *)[dst fileSystemRepresentation], kFSPathMakeRefDoNotFollowLeafSymlink, &dstRef, NULL);
 	hadFileAtDest = (err == noErr);	// There is a file at the destination, move it aside. If we normalized the name, we might not get here, so don't error.
@@ -266,9 +354,16 @@ static BOOL AuthorizationExecuteWithPrivilegesAndWait(AuthorizationRef authoriza
 			return [self _copyPathWithForcedAuthentication:src toPath:dst temporaryPath:tmpPath error:error];
 	}
 	
-	err = FSPathMakeRef((UInt8 *)[[dst stringByDeletingLastPathComponent] fileSystemRepresentation], &targetRef, NULL);
+	if( hadFileAtDest )
+	{
+		err = FSPathMakeRef((UInt8 *)[[tmpPath stringByDeletingLastPathComponent] fileSystemRepresentation], &tmpDirRef, NULL);
+		if (err != noErr)
+			err = FSPathMakeRef((UInt8 *)[[dst stringByDeletingLastPathComponent] fileSystemRepresentation], &tmpDirRef, NULL);
+	}
+	
+	err = FSPathMakeRef((UInt8 *)[[dst stringByDeletingLastPathComponent] fileSystemRepresentation], &dstDirRev, NULL);
 	if (err == noErr)
-		err = FSMoveObjectSync(&dstRef, &targetRef, (CFStringRef)tmp, &movedRef, 0);
+		err = FSMoveObjectSync(&dstRef, &tmpDirRef, (CFStringRef)[tmpPath lastPathComponent], &movedRef, 0);
 	if (err != noErr && hadFileAtDest)
 	{
 		if (error != NULL)
@@ -277,30 +372,17 @@ static BOOL AuthorizationExecuteWithPrivilegesAndWait(AuthorizationRef authoriza
 	}
 	err = FSPathMakeRef((UInt8 *)[src fileSystemRepresentation], &srcRef, NULL);
 	if (err == noErr)
-		err = FSCopyObjectSync(&srcRef, &targetRef, (CFStringRef)[dst lastPathComponent], NULL, 0);
+		err = FSCopyObjectSync(&srcRef, &dstDirRev, (CFStringRef)[dst lastPathComponent], NULL, 0);
 	if (err != noErr)
 	{
 		// We better move the old version back to its old location
 		if( hadFileAtDest )
-			FSMoveObjectSync(&movedRef, &targetRef, (CFStringRef)[dst lastPathComponent], &movedRef, 0);
+			FSMoveObjectSync(&movedRef, &dstDirRev, (CFStringRef)[dst lastPathComponent], &movedRef, 0);
 		if (error != NULL)
 			*error = [NSError errorWithDomain:SUSparkleErrorDomain code:SUFileCopyFailure userInfo:[NSDictionary dictionaryWithObject:[NSString stringWithFormat:@"Couldn't copy %@ to %@.", src, dst] forKey:NSLocalizedDescriptionKey]];
 		return NO;			
 	}
-	
-	// Trash the old copy of the app.
-	if( hadFileAtDest )
-	{
-#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_4
-		if (FSMoveObjectToTrashSync == NULL)
-			[self performSelectorOnMainThread:@selector(_movePathToTrash:) withObject:tmpPath waitUntilDone:YES];
-		else if (noErr != FSMoveObjectToTrashSync(&movedRef, NULL, 0))
-			NSLog(@"Sparkle error: couldn't move %@ to the trash. This is often a sign of a permissions error.", tmpPath);
-#else
-		[self performSelectorOnMainThread:@selector(_movePathToTrash:) withObject:tmpPath waitUntilDone:YES];
-#endif
-	}
-	
+		
 	// If the currently-running application is trusted, the new
 	// version should be trusted as well.  Remove it from the
 	// quarantine to avoid a delay at launch, and to avoid
