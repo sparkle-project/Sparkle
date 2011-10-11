bb682da743fbeb65fcc6d7def976199f736ef288
Andy Matuschak <andy@andymatuschak.org>
2011-10-10 22:44:17 -0700

    Prompts the user to update after a week (rather than a day) if he doesn't quit the app.

--- SUAutomaticUpdateDriver.m
+++ SUAutomaticUpdateDriver.m
@@ -12,8 +12,8 @@
 #import "SUHost.h"
 #import "SUConstants.h"
 
-// If the user hasn't quit in a day, ask them if they want to relaunch to get the latest bits. It doesn't matter that this measure of "one day" is imprecise.
-static const NSTimeInterval SUAutomaticUpdatePromptImpatienceTimer = 60 * 60 * 24;
+// If the user hasn't quit in a week, ask them if they want to relaunch to get the latest bits. It doesn't matter that this measure of "one day" is imprecise.
+static const NSTimeInterval SUAutomaticUpdatePromptImpatienceTimer = 60 * 60 * 24 * 7;
 
 @implementation SUAutomaticUpdateDriver
 
