47a4978e6a5e264e572c6f6d95826eeeb9c5e22d
Kornel Lesiński <kornel@geekhood.net>
2014-05-21 22:18:23 +0100

    Secure keys’ permissions

--- generate_keys.rb
+++ generate_keys.rb
@@ -8,8 +8,10 @@ done
 openssl="/usr/bin/openssl"
 $openssl dsaparam 1024 < /dev/urandom > dsaparam.pem
 $openssl gendsa dsaparam.pem -out dsa_priv.pem
+chmod 0400 dsa_priv.pem
 $openssl dsa -in dsa_priv.pem -pubout -out dsa_pub.pem
 rm dsaparam.pem
+chmod 0444 dsa_pub.pem
 echo "
 Generated private and public keys: dsa_priv.pem and dsa_pub.pem.
 BACK UP YOUR PRIVATE KEY AND KEEP IT SAFE!
