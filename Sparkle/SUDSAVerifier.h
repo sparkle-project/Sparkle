ae728448474b85ed4c727f3c6babf21e52797c34
Zachary Waldowski <zach@waldowski.me>
2014-06-14 21:47:22 -0400

    Make SUDSAVerifier forward-compatible with Lion security

--- Sparkle/SUDSAVerifier.h
+++ Sparkle/SUDSAVerifier.h
@@ -5,6 +5,12 @@
 //  Created by Andy Matuschak on 3/16/06.
 //  Copyright 2006 Andy Matuschak. All rights reserved.
 //
+//  Includes code by Zach Waldowski on 10/18/13.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+//  Includes code from Plop by Mark Hamlin.
+//  Copyright (c) 2011 Mark Hamlin. All rights reserved.
+//
 
 #ifndef SUDSAVERIFIER_H
 #define SUDSAVERIFIER_H
@@ -12,7 +18,16 @@
 #import <Cocoa/Cocoa.h>
 
 @interface SUDSAVerifier : NSObject
+
 + (BOOL)validatePath:(NSString *)path withEncodedDSASignature:(NSString *)encodedSignature withPublicDSAKey:(NSString *)pkeyString;
+
+- (instancetype)initWithPublicKeyData:(NSData *)data;
+- (instancetype)initWithPublicKeyString:(NSString *)string;
+
+- (BOOL)verifyURL:(NSURL *)URL signature:(NSData *)signature;
+- (BOOL)verifyFileAtPath:(NSString *)path signature:(NSData *)signature;
+- (BOOL)verifyStream:(NSInputStream *)stream signature:(NSData *)signature;
+
 @end
 
 #endif
