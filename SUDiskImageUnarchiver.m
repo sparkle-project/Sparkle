135bcb483e6c14008ee1091d34ee374da8a12938
Jake Petroules <jake.petroules@petroules.com>
2014-06-28 21:11:43 -0400

    Let the compiler know that SULog is a format function.
    This allows it to warn properly on malformed format strings.

--- SUDiskImageUnarchiver.m
+++ SUDiskImageUnarchiver.m
@@ -80,7 +80,7 @@
 		if (taskResult != 0)
 		{
 			NSString*	resultStr = output ? [[[NSString alloc] initWithData: output encoding: NSUTF8StringEncoding] autorelease] : nil;
-			SULog( @"hdiutil failed with code: %d data: <<%@>>", taskResult, resultStr );
+			SULog( @"hdiutil failed with code: %ld data: <<%@>>", (long)taskResult, resultStr );
 			goto reportError;
 		}
 		mountedSuccessfully = YES;
