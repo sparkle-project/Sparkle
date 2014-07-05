6826505d3ed6d6b11d12e14fb0a119258fc2e192
Kornel Lesiński <kornel@geekhood.net>
2014-07-05 01:19:13 +0100

    Apply clang-format rules

--- Sparkle/SUDSAVerifier.m
+++ Sparkle/SUDSAVerifier.m
@@ -16,7 +16,7 @@
 #include <CommonCrypto/CommonDigest.h>
 
 @implementation SUDSAVerifier {
-	SecKeyRef _secKey;
+    SecKeyRef _secKey;
 }
 
 + (BOOL)validatePath:(NSString *)path withEncodedDSASignature:(NSString *)encodedSignature withPublicDSAKey:(NSString *)pkeyString
@@ -25,15 +25,15 @@
         return NO;
     }
 
-	SUDSAVerifier *verifier = [[self alloc] initWithPublicKeyData:[pkeyString dataUsingEncoding:NSUTF8StringEncoding]];
+    SUDSAVerifier *verifier = [[self alloc] initWithPublicKeyData:[pkeyString dataUsingEncoding:NSUTF8StringEncoding]];
 
     if (!verifier) {
         return NO;
     }
 
-	NSString *strippedSignature = [encodedSignature stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
-	NSData *signature = [[NSData alloc] initWithBase64Encoding:strippedSignature];
-	return [verifier verifyFileAtPath:path signature:signature];
+    NSString *strippedSignature = [encodedSignature stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
+    NSData *signature = [[NSData alloc] initWithBase64Encoding:strippedSignature];
+    return [verifier verifyFileAtPath:path signature:signature];
 }
 
 - (instancetype)initWithPublicKeyData:(NSData *)data
@@ -44,30 +44,32 @@
         return nil;
     }
 
-	SecExternalFormat format = kSecFormatOpenSSL;
-	SecExternalItemType itemType = kSecItemTypePublicKey;
-	CFArrayRef items = NULL;
+    SecExternalFormat format = kSecFormatOpenSSL;
+    SecExternalItemType itemType = kSecItemTypePublicKey;
+    CFArrayRef items = NULL;
 
-	OSStatus status = SecItemImport((__bridge CFDataRef)data, NULL, &format, &itemType, 0, NULL, NULL, &items);
-	if (status != errSecSuccess || !items) {
-		if (items) {
-			CFRelease(items);
-		}
-		return nil;
-	}
+    OSStatus status = SecItemImport((__bridge CFDataRef)data, NULL, &format, &itemType, 0, NULL, NULL, &items);
+    if (status != errSecSuccess || !items) {
+        if (items) {
+            CFRelease(items);
+        }
+        return nil;
+    }
 
-	if (format == kSecFormatOpenSSL && itemType == kSecItemTypePublicKey && CFArrayGetCount(items) == 1) {
-		_secKey = (SecKeyRef)CFRetain(CFArrayGetValueAtIndex(items, 0));
-	}
+    if (format == kSecFormatOpenSSL && itemType == kSecItemTypePublicKey && CFArrayGetCount(items) == 1) {
+        _secKey = (SecKeyRef)CFRetain(CFArrayGetValueAtIndex(items, 0));
+    }
 
-	CFRelease(items);
+    CFRelease(items);
 
-	return self;
+    return self;
 }
 
 - (void)dealloc
 {
-	if (_secKey) { CFRelease(_secKey); }
+    if (_secKey) {
+        CFRelease(_secKey);
+    }
 }
 
 - (BOOL)verifyFileAtPath:(NSString *)path signature:(NSData *)signature
@@ -75,51 +77,63 @@
     if (!path.length) {
         return NO;
     }
-	NSInputStream *dataInputStream = [NSInputStream inputStreamWithFileAtPath:path];
-	return [self verifyStream:dataInputStream signature:signature];
+    NSInputStream *dataInputStream = [NSInputStream inputStreamWithFileAtPath:path];
+    return [self verifyStream:dataInputStream signature:signature];
 }
 
 - (BOOL)verifyStream:(NSInputStream *)stream signature:(NSData *)signature
 {
-	if (!stream || !signature) {
+    if (!stream || !signature) {
         return NO;
     }
 
-	__block SecGroupTransformRef group = SecTransformCreateGroupTransform();
-	__block SecTransformRef dataReadTransform = NULL;
-	__block SecTransformRef dataDigestTransform = NULL;
-	__block SecTransformRef dataVerifyTransform = NULL;
-	__block CFErrorRef error = NULL;
+    __block SecGroupTransformRef group = SecTransformCreateGroupTransform();
+    __block SecTransformRef dataReadTransform = NULL;
+    __block SecTransformRef dataDigestTransform = NULL;
+    __block SecTransformRef dataVerifyTransform = NULL;
+    __block CFErrorRef error = NULL;
 
-	BOOL(^cleanup)(void) = ^{
+    BOOL (^cleanup)(void) = ^{
 		if (group) CFRelease(group);
 		if (dataReadTransform) CFRelease(dataReadTransform);
 		if (dataDigestTransform) CFRelease(dataDigestTransform);
 		if (dataVerifyTransform) CFRelease(dataVerifyTransform);
 		if (error) CFRelease(error);
 		return NO;
-	};
+    };
 
-	dataReadTransform = SecTransformCreateReadTransformWithReadStream((__bridge CFReadStreamRef)stream);
-	if (!dataReadTransform) { return cleanup(); }
+    dataReadTransform = SecTransformCreateReadTransformWithReadStream((__bridge CFReadStreamRef)stream);
+    if (!dataReadTransform) {
+        return cleanup();
+    }
 
-	dataDigestTransform = SecDigestTransformCreate(kSecDigestSHA1, CC_SHA1_DIGEST_LENGTH, NULL);
-	if (!dataDigestTransform) { return cleanup(); }
+    dataDigestTransform = SecDigestTransformCreate(kSecDigestSHA1, CC_SHA1_DIGEST_LENGTH, NULL);
+    if (!dataDigestTransform) {
+        return cleanup();
+    }
 
-	dataVerifyTransform = SecVerifyTransformCreate(_secKey, (__bridge CFDataRef)signature, NULL);
-	if (!dataVerifyTransform) { return cleanup(); }
+    dataVerifyTransform = SecVerifyTransformCreate(_secKey, (__bridge CFDataRef)signature, NULL);
+    if (!dataVerifyTransform) {
+        return cleanup();
+    }
 
-	SecTransformConnectTransforms(dataReadTransform, kSecTransformOutputAttributeName, dataDigestTransform, kSecTransformInputAttributeName, group, &error);
-	if (error) { return cleanup(); }
+    SecTransformConnectTransforms(dataReadTransform, kSecTransformOutputAttributeName, dataDigestTransform, kSecTransformInputAttributeName, group, &error);
+    if (error) {
+        return cleanup();
+    }
 
-	SecTransformConnectTransforms(dataDigestTransform, kSecTransformOutputAttributeName, dataVerifyTransform, kSecTransformInputAttributeName, group, &error);
-	if (error) { return cleanup(); }
+    SecTransformConnectTransforms(dataDigestTransform, kSecTransformOutputAttributeName, dataVerifyTransform, kSecTransformInputAttributeName, group, &error);
+    if (error) {
+        return cleanup();
+    }
 
-	NSNumber *result = CFBridgingRelease(SecTransformExecute(group, &error));
-	if (error) { return cleanup(); }
+    NSNumber *result = CFBridgingRelease(SecTransformExecute(group, &error));
+    if (error) {
+        return cleanup();
+    }
 
-	cleanup();
-	return result.boolValue;
+    cleanup();
+    return result.boolValue;
 }
 
 @end
