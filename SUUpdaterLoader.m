aa916f4bfe4742460ff742d6604b25623920dc54
Dmytro Tretiakov <dimaty@macpaw.com>
2014-10-08 19:33:11 +0300

    Updated SUUpdaterLoader class with new load methods.

--- SUUpdaterLoader.m
+++ SUUpdaterLoader.m
@@ -11,9 +11,7 @@
 
 // This is a proxy for DM_SUUpdater class used in DevMateSparkle_lib target.
 //  In cases when developer uses this lib in it's own project but forgot to change
-//  updater object class inside XIB file from SUUpdater to DM_SUUpdater updating process will be broken
-//  with additional console log about absence of SUUpdater class. This class implementation will
-//  log additional helpful info to console
+//  updater object class inside XIB file from SUUpdater to DM_SUUpdater updating process won't be broken.
 
 static NSString *sOriginalSUUpdaterClass = @"SUUpdater";
 
@@ -29,32 +27,55 @@ static NSString *sOriginalSUUpdaterClass = @"SUUpdater";
         // Custom Sparkle.framework is absent
         // Add own SUUpdater proxy class
         Class SUUpdaterClass = objc_allocateClassPair([NSObject class], [sOriginalSUUpdaterClass UTF8String], 0);
-        
-        BOOL success = SUUpdaterClass != Nil;
-        if (success)
-        {
-            SEL selector = @selector(init);
-            IMP implementation = [self instanceMethodForSelector:selector];
-            const char *types = method_getTypeEncoding(class_getInstanceMethod(self, selector));
-
-            success = class_addMethod(SUUpdaterClass, selector, implementation, types);
-        }
-        
-        if (success)
+        if (Nil != SUUpdaterClass)
         {
             objc_registerClassPair(SUUpdaterClass);
+        }
+        Class SUUpdaterMetaclass = objc_getMetaClass(class_getName(SUUpdaterClass));
+        
+        Class LoaderClass = [self class];
+        Class LoaderMetaclass = objc_getMetaClass(class_getName(LoaderClass));
+        
+        BOOL success = SUUpdaterClass != Nil && SUUpdaterMetaclass != Nil;
+        void *runtimeMethods[][3] = {
+            { SUUpdaterClass, LoaderClass, @selector(init) },
+            { SUUpdaterMetaclass, LoaderMetaclass, @selector(sharedUpdater) },
+            { SUUpdaterMetaclass, LoaderMetaclass, @selector(updaterForBundle:) },
+        };
+        size_t methodsCount = sizeof(runtimeMethods) / sizeof(runtimeMethods[0]);
+        for (size_t i = 0; success && i < methodsCount; ++i)
+        {
+            Class changeClass = runtimeMethods[i][0];
+            Class prototypeClass = runtimeMethods[i][1];
+            SEL selector = runtimeMethods[i][2];
+            
+            IMP implementation = class_getMethodImplementation(prototypeClass, selector);
+            const char *types = method_getTypeEncoding(class_getInstanceMethod(prototypeClass, selector));
+
+            success = class_addMethod(changeClass, selector, implementation, types);
+        }
+        
+        if (!success)
+        {
+            objc_disposeClassPair(SUUpdaterClass);
         }
     }
 }
 
 - (id)init
 {
-    NSLog(@"===========================================");
-    NSLog(@"WARNING! Use new %@ class instead of %@ everywhere in your project (including XIB files)!", NSStringFromClass([SUUpdater class]), sOriginalSUUpdaterClass);
-    NSLog(@"===========================================");
-
     [self release];
-    return (id)[SUUpdaterWorking sharedUpdater];
+    return (id)[[SUUpdaterWorking alloc] init];
+}
+
++ (id)sharedUpdater
+{
+    return [SUUpdaterWorking sharedUpdater];
+}
+
++ (id)updaterForBundle:(NSBundle *)bundle
+{
+    return [SUUpdaterWorking updaterForBundle:bundle];
 }
 
 @end
