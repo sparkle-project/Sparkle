e294e85a4491aa9fb02f67131c5c7d7ec7154aab
Sam Deane <sam@elegantchaos.com>
2015-06-11 14:26:39 +0100

    Fixed another XC7 warning.

--- finish_installation.m
+++ finish_installation.m
@@ -120,7 +120,7 @@
         else
             appPath = [host installationPath];
         
-        NSRunningApplication *relaunchedApplication = [[NSWorkspace sharedWorkspace] launchApplicationAtURL:[NSURL fileURLWithPath:appPath] options:NSWorkspaceLaunchDefault configuration:nil error:NULL];
+        NSRunningApplication *relaunchedApplication = [[NSWorkspace sharedWorkspace] launchApplicationAtURL:[NSURL fileURLWithPath:appPath] options:NSWorkspaceLaunchDefault configuration:@{} error:NULL];
         [relaunchedApplication activateWithOptions:NSApplicationActivateIgnoringOtherApps];
     }
 
