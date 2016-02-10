934398c35a33eb48d2868b27961b62319ec65988
Sam Deane <sam@elegantchaos.com>
2016-02-10 10:02:05 +0000

    Applied patch @70f6929ac766b404e8e0d28d5cbda7872dc2ee3f from the main Sparkle tree, to prevent man-in-the-middle attacks from opening files etc.
    (this is still not foolproof - a man in the middle attack could do plenty of mischief with the wrong http/https url)

--- SUUpdateAlert.m
+++ SUUpdateAlert.m
@@ -305,6 +305,16 @@
 
 - (void)webView:sender decidePolicyForNavigationAction:(NSDictionary *)actionInformation request:(NSURLRequest *)request frame:frame decisionListener:listener
 {
+    NSURL *requestURL = request.URL;
+    NSString *scheme = requestURL.scheme;
+    BOOL whitelistedSafe = [@"http" isEqualToString:scheme] || [@"https" isEqualToString:scheme] || [@"about:blank" isEqualToString:requestURL.absoluteString];
+
+    // Do not allow redirects to dangerous protocols such as file://
+    if (!whitelistedSafe) {
+        [listener ignore];
+        return;
+    }
+
     if (webViewFinishedLoading) {
         [[NSWorkspace sharedWorkspace] openURL:[request URL]];
 		
