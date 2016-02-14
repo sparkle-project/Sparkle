7ca99547b9b14eba97c822337674dfd45a12a4c5
Zorg <zorgiepoo@gmail.com>
2016-02-14 13:18:32 -0500

    Add note about default icon assumption potentially being off

--- Sparkle/SUHost.m
+++ Sparkle/SUHost.m
@@ -158,6 +158,8 @@
     NSImage *icon = [[NSImage alloc] initWithContentsOfFile:iconPath];
     // Use a default icon if none is defined.
     if (!icon) {
+        // this asumption may not be correct (eg. even though we're not the main bundle, it could be still be a regular app)
+        // but still better than nothing if no icon was included
         BOOL isMainBundle = (self.bundle == [NSBundle mainBundle]);
 
         NSString *fileType = isMainBundle ? (__bridge NSString *)kUTTypeApplication : (__bridge NSString *)kUTTypeBundle;
