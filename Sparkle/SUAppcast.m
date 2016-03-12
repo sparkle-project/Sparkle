25b451b4c216a623120bb304411bbb051c239861
Zorg <zorgiepoo@gmail.com>
2016-03-12 14:22:39 -0500

    Remove unused #imports

--- Sparkle/SUAppcast.m
+++ Sparkle/SUAppcast.m
@@ -6,8 +6,6 @@
 //  Copyright 2006 Andy Matuschak. All rights reserved.
 //
 
-#import "SUUpdater.h"
-
 #import "SUAppcast.h"
 #import "SUAppcastItem.h"
 #import "SUVersionComparisonProtocol.h"
--- Sparkle/SUConstants.m
+++ Sparkle/SUConstants.m
@@ -6,11 +6,6 @@
 //  Copyright 2006 Andy Matuschak. All rights reserved.
 //
 
-#import "SUUpdater.h"
-
-#import "SUAppcast.h"
-#import "SUAppcastItem.h"
-#import "SUVersionComparisonProtocol.h"
 #import "SUConstants.h"
 #import "SUErrors.h"
 
