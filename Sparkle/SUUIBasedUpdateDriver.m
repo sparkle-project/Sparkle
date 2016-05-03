aae539bc9b30d42a85fa3227581931262ae9e0c1
Marcus Osobase <marcosobase@yahoo.com>
2016-05-03 11:54:36 -0400

    Dont show update window

--- Sparkle/SUUIBasedUpdateDriver.m
+++ Sparkle/SUUIBasedUpdateDriver.m
@@ -61,29 +61,29 @@
         return;
     }
 
-    self.updateAlert = [[SUUpdateAlert alloc] initWithAppcastItem:self.updateItem host:self.host completionBlock:^(SUUpdateAlertChoice choice) {
-        [self updateAlertFinishedWithChoice:choice];
-    }];
-
-    id<SUVersionDisplay> versDisp = nil;
-    if ([[self.updater delegate] respondsToSelector:@selector(versionDisplayerForUpdater:)]) {
-        versDisp = [[self.updater delegate] versionDisplayerForUpdater:self.updater];
-    }
-    [self.updateAlert setVersionDisplayer:versDisp];
-
-    // If the app is a menubar app or the like, we need to focus it first and alter the
-    // update prompt to behave like a normal window. Otherwise if the window were hidden
-    // there may be no way for the application to be activated to make it visible again.
-    if ([self.host isBackgroundApplication]) {
-        [[self.updateAlert window] setHidesOnDeactivate:NO];
-        [NSApp activateIgnoringOtherApps:YES];
-    }
-
-    // Only show the update alert if the app is active; otherwise, we'll wait until it is.
-    if ([NSApp isActive])
-        [[self.updateAlert window] makeKeyAndOrderFront:self];
-    else
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive:) name:NSApplicationDidBecomeActiveNotification object:NSApp];
+//    self.updateAlert = [[SUUpdateAlert alloc] initWithAppcastItem:self.updateItem host:self.host completionBlock:^(SUUpdateAlertChoice choice) {
+//        [self updateAlertFinishedWithChoice:choice];
+//    }];
+//
+//    id<SUVersionDisplay> versDisp = nil;
+//    if ([[self.updater delegate] respondsToSelector:@selector(versionDisplayerForUpdater:)]) {
+//        versDisp = [[self.updater delegate] versionDisplayerForUpdater:self.updater];
+//    }
+//    [self.updateAlert setVersionDisplayer:versDisp];
+//
+//    // If the app is a menubar app or the like, we need to focus it first and alter the
+//    // update prompt to behave like a normal window. Otherwise if the window were hidden
+//    // there may be no way for the application to be activated to make it visible again.
+//    if ([self.host isBackgroundApplication]) {
+//        [[self.updateAlert window] setHidesOnDeactivate:NO];
+//        [NSApp activateIgnoringOtherApps:YES];
+//    }
+//
+//    // Only show the update alert if the app is active; otherwise, we'll wait until it is.
+//    if ([NSApp isActive])
+//        [[self.updateAlert window] makeKeyAndOrderFront:self];
+//    else
+//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive:) name:NSApplicationDidBecomeActiveNotification object:NSApp];
 }
 
 - (void)didNotFindUpdate
