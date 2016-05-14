95416e4f6551278b1fa792e438c5c39f2e751bf7
Zorg <zorgiepoo@gmail.com>
2016-05-13 22:11:47 -0400

    Add SUURLDownload specifically for downloading temporary files

---
+++ Sparkle/SUURLDownload.m
@@ -0,0 +1,60 @@
+//
+//  SUURLDownload.m
+//  Sparkle
+//
+//  Created by Mayur Pawashe on 5/13/16.
+//  Copyright © 2016 Sparkle Project. All rights reserved.
+//
+
+#import "SUURLDownload.h"
+#import "SUXPCServiceInfo.h"
+#import "SUAppcastDownloader.h"
+#import "SUAppcastDownloaderProtocol.h"
+
+void SUDownloadURLWithRequest(NSURLRequest * request, void (^completionBlock)(NSData * _Nullable, NSError * _Nullable))
+{
+    id<SUAppcastDownloaderProtocol> downloader = nil;
+    NSXPCConnection *connection = nil;
+    __block BOOL retrievedDownloadResult = NO;
+    
+    if (!SUXPCServiceExists(@APPCAST_DOWNLOADER_PRODUCT_NAME)) {
+        downloader = [[SUAppcastDownloader alloc] init];
+    } else {
+        connection = [[NSXPCConnection alloc] initWithServiceName:@APPCAST_DOWNLOADER_BUNDLE_ID];
+        connection.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(SUAppcastDownloaderProtocol)];
+        
+        __weak NSXPCConnection *weakConnection = connection;
+        connection.interruptionHandler = ^{
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (!retrievedDownloadResult) {
+                    [weakConnection invalidate];
+                }
+            });
+        };
+        
+        connection.invalidationHandler = ^{
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (!retrievedDownloadResult) {
+                    completionBlock(nil, nil);
+                }
+            });
+        };
+        
+        [connection resume];
+        
+        downloader = connection.remoteObjectProxy;
+    }
+    
+    [downloader startDownloadWithRequest:request completion:^(NSData * _Nullable data, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            retrievedDownloadResult = YES;
+            [connection invalidate];
+            
+            if (data == nil) {
+                completionBlock(nil, error);
+            } else {
+                completionBlock(data, nil);
+            }
+        });
+    }];
+}
