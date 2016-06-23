9944f2496c9627ab111eb43a5da68e430a7c0d63
Kornel Lesiński <kornel@geekhood.net>
2016-06-23 17:24:09 +0100

    Comment

--- Sparkle/SUBasicUpdateDriver.m
+++ Sparkle/SUBasicUpdateDriver.m
@@ -319,6 +319,8 @@
     
     BOOL dsaKeysMatch = (publicDSAKey == nil || newPublicDSAKey == nil) ? NO : [publicDSAKey isEqualToString:newPublicDSAKey];
     
+    // This check is not a security measure, because the new key is not trusted.
+    // The check ensures that the app author has correctly used DSA keys, so that the app will be updateable in the next version
     if (newPublicDSAKey != nil) {
         if (![SUDSAVerifier validatePath:downloadedPath withEncodedDSASignature:DSASignature withPublicDSAKey:newPublicDSAKey]) {
             SULog(@"DSA signature validation failed. The update has a public DSA key and is signed with a DSA key, but the %@ doesn't match the signature. The update will be rejected.",
