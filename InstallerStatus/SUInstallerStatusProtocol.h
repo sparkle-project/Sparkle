320002915765e6793f3a58037a45c483ad47f321
Zorg <zorgiepoo@gmail.com>
2016-07-23 23:31:00 -0400

    Include adopted protocol methods in SUInstallerStatusProtocol
    This is because of compatibility issues with 10.8

--- InstallerStatus/SUInstallerStatusProtocol.h
+++ InstallerStatus/SUInstallerStatusProtocol.h
@@ -9,8 +9,16 @@
 #import <Foundation/Foundation.h>
 #import "SUStatusInfoProtocol.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 // The protocol that this service will vend as its API. This header file will also need to be visible to the process hosting the service.
 @protocol SUInstallerStatusProtocol <SUStatusInfoProtocol>
+
+// Even though this is declared in SUStatusInfoProtocol, we should declare it here because macOS 10.8 doesn't traverse adopted protocols
+- (void)probeStatusInfoWithReply:(void (^)(NSData * _Nullable installationInfoData))reply;
+
+// Even though this is declared in SUStatusInfoProtocol, we should declare it here because macOS 10.8 doesn't traverse adopted protocols
+- (void)probeStatusConnectivityWithReply:(void (^)(void))reply;
 
 - (void)setInvalidationHandler:(void (^)(void))invalidationHandler;
 
@@ -19,3 +27,5 @@
 - (void)invalidate;
     
 @end
+
+NS_ASSUME_NONNULL_END
