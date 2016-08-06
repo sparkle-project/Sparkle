a303859f109940d8e513a7c2975930feec5f1efd
Zorg <zorgiepoo@gmail.com>
2016-08-06 10:57:53 -0400

    Add deprecation notice to interactive package installer

--- Autoupdate/SUPackageInstaller.h
+++ Autoupdate/SUPackageInstaller.h
@@ -9,6 +9,9 @@
 #import <Foundation/Foundation.h>
 #import "SPUInstallerProtocol.h"
 
+// This is the deprecated package installation type, aka the "interactive" package installer
+// For a more supported package installation, see SUGuidedPackageInstaller
+
 @interface SUPackageInstaller : NSObject <SPUInstallerProtocol>
 
 - (instancetype)initWithPackagePath:(NSString *)packagePath;
