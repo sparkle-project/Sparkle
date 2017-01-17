942d70d3aaa49af9bc8b5d50d0f782c2e08c710e
Bi11 <Bi11gates9999@gmail.com>
2017-01-18 04:53:37 +0800

    Suppress partial availability warning on line level

--- Sparkle/SUTouchBarButtonGroup.m
+++ Sparkle/SUTouchBarButtonGroup.m
@@ -23,8 +23,11 @@
     NSMutableArray<NSButton *> *buttonCopies = [NSMutableArray arrayWithCapacity:buttons.count];
 
     for (NSUInteger i = 0; i < buttons.count; i++) {
-        NSButton *button = buttons[i];
+        NSButton *button = [buttons objectAtIndex:i];
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpartial-availability"
         NSButton *buttonCopy = [NSButton buttonWithTitle:button.title target:button.target action:button.action];
+#pragma clang diagnostic pop
         buttonCopy.tag = button.tag;
         buttonCopy.enabled = button.enabled;
 
@@ -46,15 +49,18 @@
         if (i == 0) {
             [constraints addObject:[NSLayoutConstraint constraintWithItem:buttonCopy attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:buttonGroup attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0]];
         } else {
-            [constraints addObject:[NSLayoutConstraint constraintWithItem:buttonCopy attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:buttonCopies[i - 1] attribute:NSLayoutAttributeLeading multiplier:1.0 constant:(i == 1) ? -8 : -32]];
-            [constraints addObject:[NSLayoutConstraint constraintWithItem:buttonCopy attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:buttonCopies[i - 1] attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.0]];
+            [constraints addObject:[NSLayoutConstraint constraintWithItem:buttonCopy attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:[buttonCopies objectAtIndex:i - 1] attribute:NSLayoutAttributeLeading multiplier:1.0 constant:(i == 1) ? -8 : -32]];
+            [constraints addObject:[NSLayoutConstraint constraintWithItem:buttonCopy attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:[buttonCopies objectAtIndex:i - 1] attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.0]];
             constraints.lastObject.priority = 250;
         }
         if (i == buttons.count - 1) {
             [constraints addObject:[NSLayoutConstraint constraintWithItem:buttonCopy attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:buttonGroup attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0]];
         }
     }
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpartial-availability"
     [NSLayoutConstraint activateConstraints:constraints];
+#pragma clang diagnostic pop
 
     _buttons = buttonCopies;
     return self;
