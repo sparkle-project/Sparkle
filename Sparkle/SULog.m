c104c1b27ebebbc6cbd5933a9e913bb4e7812710
Kornel Lesiński <kornel@geekhood.net>
2017-01-18 22:31:03 +0000

    Fix facility identifier

--- Sparkle/SULog.m
+++ Sparkle/SULog.m
@@ -43,12 +43,12 @@ void SULog(SULogLevel level, NSString *format, ...)
             // Act the same way os_log() does; don't log to stderr if a terminal device is attached
             if (!isatty(STDERR_FILENO)) {
                 options |= ASL_OPT_STDERR;
-    }
+        }
             
             NSString *displayName = [[NSFileManager defaultManager] displayNameAtPath:mainBundle.bundlePath];
-            client = asl_open([displayName stringByAppendingString:@" [Sparkle]"].UTF8String, bundleIdentifier.UTF8String, options);
+            client = asl_open([displayName stringByAppendingString:@" [Sparkle]"].UTF8String, SPARKLE_BUNDLE_IDENTIFIER, options);
             queue = dispatch_queue_create(NULL, DISPATCH_QUEUE_SERIAL);
-}
+        }
     });
 
     if (!hasOSLogging && client == NULL) {
