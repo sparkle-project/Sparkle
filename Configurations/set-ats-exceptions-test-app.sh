f42ceec378d35becd6be702f8219ee0a537da03c
Kent Sutherland <git@ksuther.com>
2017-12-07 19:07:02 -0600

    Check for a signing identity of "-" when building Sparkle Test App

--- Configurations/set-ats-exceptions-test-app.sh
+++ Configurations/set-ats-exceptions-test-app.sh
@@ -7,6 +7,6 @@ DOWNLOADER_INFO_PLIST="${TARGET_BUILD_DIR}"/"${CONTENTS_FOLDER_PATH}"/XPCService
 #Only alter bundles if code signing is off, else we'll invalidate the signature on them
 #If code signing is enabled (as in debug mode), then we rely on another script for turning ATS exceptions on for these services
 #In release mode however, those services do not have ATS exceptions set up which is why we have to set up exceptions *here*
-if [ -z "${CODE_SIGN_IDENTITY}" ] ; then
+if [ -z "${CODE_SIGN_IDENTITY}" -o "${CODE_SIGN_IDENTITY}" == "-" ] ; then
     /usr/libexec/PlistBuddy -c "Set :NSAppTransportSecurity:NSAllowsArbitraryLoads YES" "${DOWNLOADER_INFO_PLIST}"
 fi
