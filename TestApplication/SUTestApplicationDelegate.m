9c4729ed4aa95ea4ff113b98577e5a65e4e99365
Kornel <kornel@geekhood.net>
2018-10-31 00:51:00 +0000

    Use EdDSA keys in test app

--- TestApplication/SUTestApplicationDelegate.m
+++ TestApplication/SUTestApplicationDelegate.m
@@ -10,6 +10,7 @@
 #import "SUUpdateSettingsWindowController.h"
 #import "SUFileManager.h"
 #import "SUTestWebServer.h"
+#import "../ed25519/src/ed25519.h" // run `git submodule update --init` if you get an erorr here
 
 @interface SUTestApplicationDelegate ()
 
@@ -117,32 +118,18 @@ static NSString * const UPDATED_VERSION = @"2.0";
     
     // Don't ever do this at home, kids (seriously)
     // (that is, including the private key inside of your application)
-    NSString *privateKeyPath = [mainBundle pathForResource:@"test_app_only_dsa_priv_dont_ever_do_this_for_real" ofType:@"pem"];
-    assert(privateKeyPath != nil);
-    
-    // Sign our update
-    NSTask *signUpdateTask = [[NSTask alloc] init];
-    NSString *signUpdatePath = [mainBundle pathForResource:@"sign_update" ofType:@""];
-    assert(signUpdatePath != nil);
-    signUpdateTask.launchPath = signUpdatePath;
+    const unsigned char self_sign_demo_only_insecure_hack[64] = {200, 238, 135, 84, 10, 189, 3, 193, 61, 208, 203, 30, 133, 47, 12, 22, 19, 52, 252, 99, 110, 205, 209, 94, 215, 144, 201, 70, 27, 162, 163, 108, 0, 164, 68, 184, 226, 93, 121, 199, 172, 17, 26, 64, 89, 68, 232, 41, 2, 26, 245, 175, 158, 165, 42, 55, 5, 97, 8, 243, 251, 164, 93, 9};
+    // in normal app this goes to Info.plist
+    const unsigned char public_key[32] = {121, 17, 79, 45, 155, 141, 51, 169, 188, 110, 91, 102, 182, 147, 215, 225, 252, 202, 110, 231, 200, 215, 62, 171, 40, 145, 237, 128, 130, 44, 150, 89};
+    unsigned char signature[64];
     
     NSURL *archiveURL = [serverDirectoryURL URLByAppendingPathComponent:zipName];
-    NSString *path = archiveURL.path;
-    assert(path != nil);
-    signUpdateTask.arguments = @[path, privateKeyPath];
+    NSData *archive = [NSData dataWithContentsOfURL:archiveURL];
+    assert(archive != nil);
     
-    NSPipe *outputPipe = [NSPipe pipe];
-    signUpdateTask.standardOutput = outputPipe;
+    ed25519_sign(signature, archive.bytes, archive.length, public_key, self_sign_demo_only_insecure_hack);
     
-    [signUpdateTask launch];
-    [signUpdateTask waitUntilExit];
-    
-    assert(signUpdateTask.terminationStatus == 0);
-    
-    NSData *signatureData = [outputPipe.fileHandleForReading readDataToEndOfFile];
-    NSString *signature = [[[NSString alloc] initWithData:signatureData encoding:NSUTF8StringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    
-    assert(signature != nil);
+    NSString *signatureString = [[NSData dataWithBytes:signature length:64] base64Encoding];
     
     // Obtain the file attributes to get the file size of our update later
     NSError *fileAttributesError = nil;
@@ -179,7 +166,7 @@ static NSString * const UPDATED_VERSION = @"2.0";
     NSUInteger numberOfLengthReplacements = [appcastContents replaceOccurrencesOfString:@"$INSERT_ARCHIVE_LENGTH" withString:[NSString stringWithFormat:@"%llu", archiveFileAttributes.fileSize] options:NSLiteralSearch range:NSMakeRange(0, appcastContents.length)];
     assert(numberOfLengthReplacements == 1);
     
-    NSUInteger numberOfSignatureReplacements = [appcastContents replaceOccurrencesOfString:@"$INSERT_DSA_SIGNATURE" withString:signature options:NSLiteralSearch range:NSMakeRange(0, appcastContents.length)];
+    NSUInteger numberOfSignatureReplacements = [appcastContents replaceOccurrencesOfString:@"$INSERT_EDDSA_SIGNATURE" withString:signatureString options:NSLiteralSearch range:NSMakeRange(0, appcastContents.length)];
     assert(numberOfSignatureReplacements == 1);
     
     NSError *writeAppcastError = nil;
