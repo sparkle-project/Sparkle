3e060d4a7b20677bec8a22c27f8a09ac24941a45
hofman <cmhofman@gmail.com>
2019-03-13 22:59:53 +0100

    Use SUAvailable.m in SUStatusController.m

--- Sparkle/SUStatusController.m
+++ Sparkle/SUStatusController.m
@@ -57,7 +57,7 @@ static NSString *const SUStatusControllerTouchBarIndentifier = @"" SPARKLE_BUNDL
     [[self window] setFrameAutosaveName:@"SUStatusFrame"];
     [self.progressBar setUsesThreadedAnimation:YES];
 
-    if ([SUOperatingSystem isOperatingSystemAtLeastVersion:(NSOperatingSystemVersion){10, 11, 0}]) {
+    if (SUAVAILABLE(10, 11)) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wpartial-availability"
         [self.statusTextField setFont:[NSFont monospacedDigitSystemFontOfSize:0 weight:NSFontWeightRegular]];
