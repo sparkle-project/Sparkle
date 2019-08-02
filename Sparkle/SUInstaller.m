b0d29ae7989417d7352280372a42c21c2749c5de
Jake Fenton <j.fenton@techsmith.com>
2019-08-02 09:29:12 -0400

    check if SUBundleName is set before normalizing
    When changing the name of an app, there's a couple things to do in order
    to ensure auto-updates still work:
    
    1. set SUBundle Name. This basically tells Sparkle "ey, expect an app
    of {SUBundleName}.(app|pkg) instead of the normal name."
    2. set `SPARKLE_NORMALIZE_INSTALLED_APPLICATION_NAME` in
    commonConfig.xccconfig to reset the name of the app.
    
    in (2), we always used the host's bundle name, instead of checking to
    see if SUBundleName was set, which means normalization doesn't work as
    expected during app name changes.

--- Sparkle/SUInstaller.m
+++ Sparkle/SUInstaller.m
@@ -152,8 +152,14 @@
 {
     NSBundle *bundle = host.bundle;
     assert(bundle != nil);
-    
-    NSString *normalizedAppPath = [[[bundle bundlePath] stringByDeletingLastPathComponent] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", [host objectForInfoDictionaryKey:(__bridge NSString *)kCFBundleNameKey], [[bundle bundlePath] pathExtension]]];
+   
+   NSString * baseBundleName = [host objectForInfoDictionaryKey:@"SUBundleName"];
+   
+   if (baseBundleName == nil) {
+      baseBundleName = [host objectForInfoDictionaryKey:(__bridge NSString *)kCFBundleNameKey];
+   }
+   
+    NSString *normalizedAppPath = [[[bundle bundlePath] stringByDeletingLastPathComponent] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", baseBundleName, [[bundle bundlePath] pathExtension]]];
 
     // Roundtrip string through fileSystemRepresentation to ensure it uses filesystem's Unicode normalization
     // rather than arbitrary Unicode form from Info.plist - #1017
