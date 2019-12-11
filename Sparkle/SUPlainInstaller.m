eb0774850623f045b2c65da94e3331106bb0afab
CoreCode <core-code@users.noreply.github.com>
2019-12-11 22:33:14 +0100

    Update SUPlainInstaller.m

--- Sparkle/SUPlainInstaller.m
+++ Sparkle/SUPlainInstaller.m
@@ -209,11 +209,12 @@
     }
     
     // Try to preserve Finder tags. Failure can be accepted without complaint as previous Sparkle versions did not do this at all
-    NSArray *resourceTags;
-    BOOL success = [oldTempURL getResourceValue:&resourceTags forKey:NSURLTagNamesKey error:NULL];
-    if (success && resourceTags.count)
-    {
-        [destinationAppURL setResourceValue:resourceTags forKey:NSURLTagNamesKey error:NULL];
+    if (SUAVAILABLE(10, 9)) {
+        NSArray *resourceTags;
+        BOOL success = [oldTempURL getResourceValue:&resourceTags forKey:NSURLTagNamesKey error:NULL];
+        if (success && resourceTags.count) {
+            [destinationAppURL setResourceValue:resourceTags forKey:NSURLTagNamesKey error:NULL];
+        }
     }
 
     // From here on out, we don't really need to bring up authorization if we haven't done so prior
