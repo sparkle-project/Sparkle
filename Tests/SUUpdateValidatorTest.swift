b969c31f07f7fab9b223b70521c3934b7a0e58eb
Jordan Rose <jrose@belkadan.com>
2020-06-13 21:08:33 -0700

    Test SUUpdateValidator's post-validation logic
    The actual tests here are the same as pre-validation; things only get
    complicated when validating with code signing. The setup's a little
    more involved though, since we need a supposedly-downloaded bundle to
    do anything.
    
    Also test invalid signatures, which are not always the same as absent
    signatures.

--- Tests/SUUpdateValidatorTest.swift
+++ Tests/SUUpdateValidatorTest.swift
@@ -10,28 +10,54 @@ import Foundation
 import XCTest
 
 class SUUpdateValidatorTest: XCTestCase {
-    enum SigningConfig: String {
+    enum KeyConfig: String, CaseIterable, Equatable {
         case none = "None"
         case dsaOnly = "DSAOnly"
         case edOnly = "EDOnly"
         case both = "Both"
     }
 
-    func host(keys config: SigningConfig) -> SUHost {
-        let testBundle = Bundle(for: SUUpdateValidatorTest.self)
-        let configBundleURL = testBundle.url(forResource: config.rawValue, withExtension: "bundle", subdirectory: "SUUpdateValidatorTest")!
-        return SUHost(bundle: Bundle(url: configBundleURL)!)
+    struct SignatureConfig: CaseIterable, Equatable, CustomDebugStringConvertible {
+        enum State: CaseIterable, Equatable {
+            case none, invalid, valid
+        }
+
+        var dsa: State
+        var ed: State
+
+        static let allCases: [SignatureConfig] = State.allCases.flatMap { dsaState in
+            State.allCases.map { edState in
+                SignatureConfig(dsa: dsaState, ed: edState)
+            }
+        }
+
+        var debugDescription: String {
+            return "(dsa: \(self.dsa), ed: \(self.ed))"
+        }
     }
 
-    func signatures(_ config: SigningConfig) -> SUSignatures {
-        let dsaSig = "MCwCFCIHCIYYkfZavNzTitTW5tlRp/k5AhQ40poFytqcVhIYdCxQznaXeJPJDQ=="
-        let edSig = "EIawm2YkDZ2gBfkEMF2+1VuuTeXnCGZOdnMdVgPPvDZioq7bvDayXqKkIIzSjKMmeFdcFJOHdnba5ZV60+gPBw=="
-        switch config {
-        case .none: return SUSignatures(dsa: nil, ed: nil)
-        case .dsaOnly: return SUSignatures(dsa: dsaSig, ed: nil)
-        case .edOnly: return SUSignatures(dsa: nil, ed: edSig)
-        case .both: return SUSignatures(dsa: dsaSig, ed: edSig)
+    func bundle(keys config: KeyConfig) -> Bundle {
+        let testBundle = Bundle(for: SUUpdateValidatorTest.self)
+        let configBundleURL = testBundle.url(forResource: config.rawValue, withExtension: "bundle", subdirectory: "SUUpdateValidatorTest")!
+        return Bundle(url: configBundleURL)!
+    }
+
+    func signatures(_ config: SignatureConfig) -> SUSignatures {
+        let dsaSig: String?
+        switch config.dsa {
+        case .none: dsaSig = nil
+        case .invalid: dsaSig = "MCwCFCIHCiYYkfZavNzTitTW5tlRp/k5AhQ40poFytqcVhIYdCxQznaXeJPJDQ=="
+        case .valid: dsaSig = "MCwCFCIHCIYYkfZavNzTitTW5tlRp/k5AhQ40poFytqcVhIYdCxQznaXeJPJDQ=="
         }
+
+        let edSig: String?
+        switch config.ed {
+        case .none: edSig = nil
+        case .invalid: edSig = "wTcpXCgWoa4NrJpsfzS61FXJIbv963//12U2ef9xstzVOLPHYK2N4/ojgpDV5N1/NGG1uWMBgK+kEWp0Z5zMDQ=="
+        case .valid: edSig = "EIawm2YkDZ2gBfkEMF2+1VuuTeXnCGZOdnMdVgPPvDZioq7bvDayXqKkIIzSjKMmeFdcFJOHdnba5ZV60+gPBw=="
+        }
+
+        return SUSignatures(dsa: dsaSig, ed: edSig)
     }
 
     var signedTestFilePath: String {
@@ -39,35 +65,42 @@ class SUUpdateValidatorTest: XCTestCase {
         return testBundle.path(forResource: "signed-test-file", ofType: "txt")!
     }
 
-    func testPrevalidation(keys keysConfig: SigningConfig, signatures signatureConfig: SigningConfig, expectedResult: Bool, line: UInt = #line) {
-        let host = self.host(keys: keysConfig)
+    func testPrevalidation(keys keysConfig: KeyConfig, signatures signatureConfig: SignatureConfig, expectedResult: Bool, line: UInt = #line) {
+        let host = SUHost(bundle: self.bundle(keys: keysConfig))!
         let signatures = self.signatures(signatureConfig)
 
         let validator = SUUpdateValidator(downloadPath: self.signedTestFilePath, signatures: signatures, host: host)
 
         let result = validator.validateDownloadPath()
-        XCTAssertEqual(result, expectedResult, line: line)
+        XCTAssertEqual(result, expectedResult, "keys: \(keysConfig), signatures: \(signatureConfig)", line: line)
     }
 
     func testPrevalidation() {
-        testPrevalidation(keys: .none, signatures: .none, expectedResult: false)
-        testPrevalidation(keys: .none, signatures: .dsaOnly, expectedResult: false)
-        testPrevalidation(keys: .none, signatures: .edOnly, expectedResult: false)
-        testPrevalidation(keys: .none, signatures: .both, expectedResult: false)
+        for signatureConfig in SignatureConfig.allCases {
+            testPrevalidation(keys: .none, signatures: signatureConfig, expectedResult: false)
+            testPrevalidation(keys: .dsaOnly, signatures: signatureConfig, expectedResult: signatureConfig.dsa == .valid)
+            testPrevalidation(keys: .edOnly, signatures: signatureConfig, expectedResult: signatureConfig.ed == .valid)
+            testPrevalidation(keys: .both, signatures: signatureConfig, expectedResult: signatureConfig.dsa == .valid && signatureConfig.ed != .invalid)
+        }
+    }
 
-        testPrevalidation(keys: .dsaOnly, signatures: .none, expectedResult: false)
-        testPrevalidation(keys: .dsaOnly, signatures: .dsaOnly, expectedResult: true)
-        testPrevalidation(keys: .dsaOnly, signatures: .edOnly, expectedResult: false)
-        testPrevalidation(keys: .dsaOnly, signatures: .both, expectedResult: true)
+    func testPostValidationWithoutCodeSigning(keys keysConfig: KeyConfig, signatures signatureConfig: SignatureConfig, expectedResult: Bool, line: UInt = #line) {
+        let bundle = self.bundle(keys: keysConfig)
+        let host = SUHost(bundle: bundle)!
+        let signatures = self.signatures(signatureConfig)
 
-        testPrevalidation(keys: .edOnly, signatures: .none, expectedResult: false)
-        testPrevalidation(keys: .edOnly, signatures: .dsaOnly, expectedResult: false)
-        testPrevalidation(keys: .edOnly, signatures: .edOnly, expectedResult: true)
-        testPrevalidation(keys: .edOnly, signatures: .both, expectedResult: true)
+        let validator = SUUpdateValidator(downloadPath: self.signedTestFilePath, signatures: signatures, host: host)
 
-        testPrevalidation(keys: .both, signatures: .none, expectedResult: false)
-        testPrevalidation(keys: .both, signatures: .dsaOnly, expectedResult: true)
-        testPrevalidation(keys: .both, signatures: .edOnly, expectedResult: false)
-        testPrevalidation(keys: .both, signatures: .both, expectedResult: true)
+        let result = validator.validate(withUpdateDirectory: bundle.bundleURL.deletingLastPathComponent().path)
+        XCTAssertEqual(result, expectedResult, "keys: \(keysConfig), signatures: \(signatureConfig)", line: line)
+    }
+
+    func testPostValidationWithoutCodeSigning() {
+        for signatureConfig in SignatureConfig.allCases {
+            testPostValidationWithoutCodeSigning(keys: .none, signatures: signatureConfig, expectedResult: false)
+            testPostValidationWithoutCodeSigning(keys: .dsaOnly, signatures: signatureConfig, expectedResult: signatureConfig.dsa == .valid)
+            testPostValidationWithoutCodeSigning(keys: .edOnly, signatures: signatureConfig, expectedResult: signatureConfig.ed == .valid)
+            testPostValidationWithoutCodeSigning(keys: .both, signatures: signatureConfig, expectedResult: signatureConfig.dsa == .valid && signatureConfig.ed != .invalid)
+        }
     }
 }
