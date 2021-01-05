d21a303b2e2c6db8472217e2120c9b308576726b
Jonas Zaugg <jonas.zaugg@yahoo.com>
2021-01-05 13:07:11 +0100

    Update release-move-tag.sh
    Prevents re-tagging when there are more than one commits since latest tag (to prevent accidentally moving an old tag)

--- Configurations/release-move-tag.sh
+++ Configurations/release-move-tag.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e
 
-# Finishing up - force re-tag
+# Convenience script to automatically commit Package.swift after updating the checksum and move the latest tag
 latest_git_tag=$(git describe --abbrev=0) # gets the latest tag name
-read -p "Do you want to commit changes to Package.swift and force move tag '$latest_git_tag'? (required for Swift Package Manager release) " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    long_message=$(git tag -n99 -l $latest_git_tag) # gets corresponding message
-    long_message=${long_message/$latest_git_tag} # trims tag name
-    long_message="$(echo -e "${long_message}" | sed -e 's/^[[:space:]]*//')" # trim leading whitespace
-    git add Package.swift
-    git commit -m "Update Package.swift"
-    git tag -fa $latest_git_tag -m "${long_message}"
+commits_since_tag=$(git rev-list ${latest_git_tag}.. --count)
+if [ "$commits_since_tag" -ge 1 ]; then
+    # If there has been more than one commits since the latest tag, it's highly likely that we did not intend to do a full release
+    echo "WARNING: More than one commit ($commits_since_tag) since tag '$latest_git_tag'. Did you tag a new version?"
+    echo "Package.swift has not been commited and tag has not been moved."
+else
+    # TODO: add sanity check to see if version is actually being updated or not?
+    read -p "Do you want to commit changes to Package.swift and force move tag '$latest_git_tag'? (required for Swift Package Manager release) " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        long_message=$(git tag -n99 -l $latest_git_tag) # gets corresponding message
+        long_message=${long_message/$latest_git_tag} # trims tag name
+        long_message="$(echo -e "${long_message}" | sed -e 's/^[[:space:]]*//')" # trim leading whitespace
+        git add Package.swift
+        git commit -m "Update Package.swift"
+        git tag -fa $latest_git_tag -m "${long_message}"
+    fi
 fi
