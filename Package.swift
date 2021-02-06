07b044dd8f304b3d05a6242a3f4c55ffb08d3f61
Kornel <kornel@geekhood.net>
2021-02-06 02:07:55 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "1.24.0"
+let version = "1.25.0-rc2"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "1.24.0"
-let checksum = "a9ae129446739a685d2fb60b392606df020ee8e8d132dcc3a5959a8dd0775f35"
+let tag = "1.25.0-rc2"
+let checksum = "9f98d6e97c8c2e7f63e7322951ae42183682ab090eb025d75cf24c072ec423f4"
 let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
