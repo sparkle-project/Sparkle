b5c9539f16de5b8647418fc467c6ca8fd5c8d22c
Kornel <kornel@geekhood.net>
2021-02-22 09:46:32 +0000

    Fight SPM retagging

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "1.26.0"
+let version = "1.26.0-a1"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "1.26.0"
-let checksum = "14f3a80c528d6ac5d6beff7ffda7e0ef6be83591d66cdd006fe55520fc6bbbf9"
+let tag = "1.26.0-a1"
+let checksum = "6995034093f9ab039a4249c331a432700ea64b90e11f2d88d36585a6df42c95c"
 let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
