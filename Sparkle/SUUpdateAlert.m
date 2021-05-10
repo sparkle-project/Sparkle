1f9d76bc13eb482a85feee4115ae81719d4f9bcb
Zorg <zorgiepoo@gmail.com>
2021-05-09 23:16:01 -0700

    Retitle install button to "Install Upgrade" for major updates

--- Sparkle/SUUpdateAlert.m
+++ Sparkle/SUUpdateAlert.m
@@ -368,6 +368,9 @@ static NSString *const SUUpdateAlertTouchBarIndentifier = @"" SPARKLE_BUNDLE_IDE
         
         // We should be explicit that the update will be installed on quit
         self.laterButton.title = SULocalizedString(@"Install on Quit", @"Alternate title for 'Remind Me Later' button when downloaded updates can be resumed");
+    } else if (self.state.majorUpgrade) {
+        // Use "upgrade" instead of "update" to signify a major change
+        self.installButton.title = SULocalizedString(@"Install Upgrade", nil);
     }
     
     if (self.state.majorUpgrade) {
