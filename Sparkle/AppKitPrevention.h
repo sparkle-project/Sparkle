4825ac5d629bee7dd13f108310834a74df974a94
Zorg <zorgiepoo@gmail.com>
2021-06-06 19:31:30 -0700

    Remove outdated comments

--- Sparkle/AppKitPrevention.h
+++ Sparkle/AppKitPrevention.h
@@ -11,10 +11,6 @@
 
 // If this error is triggered, you can have Xcode indicate to you which source file including this header caused the issue
 
-// One may wonder for certain targets where AppKit is banned completely, why just not link AppKit instead.
-// Well, Xcode has thing called auto-linking that I don't trust very much.
-// Even after trying to disable the setting, I've had targets able to link to AppKit without AppKit being specified in the list of linked libraries.
-
 #ifdef _APPKITDEFINES_H
 #error This is a core or daemon-safe module and should NOT import AppKit
 #endif
