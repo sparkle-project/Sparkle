686fd05fce33c2a6e1f9d77eb0d232d293ff508d
Zorg <zorgiepoo@gmail.com>
2021-06-20 23:11:02 -0700

    Add tests for informational only updates

--- Tests/SUAppcastTest.swift
+++ Tests/SUAppcastTest.swift
@@ -116,6 +116,69 @@ class SUAppcastTest: XCTestCase {
         }
     }
     
+    func testInformationalUpdateVersions() {
+        let testURL = Bundle(for: SUAppcastTest.self).url(forResource: "testappcast_info_updates", withExtension: "xml")!
+        
+        do {
+            let testData = try Data(contentsOf: testURL)
+            
+            let versionComparator = SUStandardVersionComparator.default()
+            
+            // Test informational updates from version 1.0
+            do {
+                let hostVersion = "1.0"
+                let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
+                
+                let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+                
+                XCTAssertFalse(appcast.items[0].isInformationOnlyUpdate)
+                XCTAssertFalse(appcast.items[1].isInformationOnlyUpdate)
+                XCTAssertTrue(appcast.items[2].isInformationOnlyUpdate)
+                XCTAssertTrue(appcast.items[3].isInformationOnlyUpdate)
+                
+                // Test delta updates inheriting informational only updates
+                do {
+                    let deltaUpdate = appcast.items[2].deltaUpdates!["2.0"] as! SUAppcastItem
+                    XCTAssertTrue(deltaUpdate.isInformationOnlyUpdate)
+                }
+            }
+            
+            // Test informational updates from version 2.4
+            do {
+                let hostVersion = "2.4"
+                let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
+                
+                let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+                
+                XCTAssertTrue(appcast.items[1].isInformationOnlyUpdate)
+            }
+            
+            // Test informational updates from version 2.5
+            do {
+                let hostVersion = "2.5"
+                let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
+                
+                let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+                
+                XCTAssertTrue(appcast.items[1].isInformationOnlyUpdate)
+            }
+            
+            // Test informational updates from version 2.6
+            do {
+                let hostVersion = "2.6"
+                let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
+                
+                let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+                
+                XCTAssertFalse(appcast.items[1].isInformationOnlyUpdate)
+            }
+            
+        } catch let err as NSError {
+            NSLog("%@", err)
+            XCTFail(err.localizedDescription)
+        }
+    }
+    
     func testMinimumAutoupdateVersion() {
         let testURL = Bundle(for: SUAppcastTest.self).url(forResource: "testappcast_minimumAutoupdateVersion", withExtension: "xml")!
         
