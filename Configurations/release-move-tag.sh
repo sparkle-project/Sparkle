1131e173e0edb69a3af500ee9bea80b082f89a23
Tony Arnold <tony@thecocoabots.com>
2021-07-26 21:21:17 +1000

    Use the makefile, and push the moved tag to GitHub as necessary

--- Configurations/release-move-tag.sh
+++ Configurations/release-move-tag.sh
@@ -10,7 +10,7 @@ function move_tag() {
     long_message=${long_message/$latest_git_tag} # trims tag name
     long_message="$(echo -e "${long_message}" | sed -e 's/^[[:space:]]*//')" # trim leading whitespace
     git add Package.swift
-    git commit -m "Update Package.swift"
+    git commit -m "Update Package.swift for version ${latest_git_tag}"
     git tag -fa $latest_git_tag -m "${long_message}"
     echo "Package.swift committed and tag '$latest_git_tag' moved."
 }
@@ -21,8 +21,23 @@ function move_tag() {
 #     echo "Package.swift has not been committed and tag has not been moved."
 # else
     # TODO: add sanity check to see if version is actually being updated or not?
+
+    if [[ ${GITHUB_TOKEN+x} ]]; then 
+        # We're running on GitHub Actions, so setup the actions username and a remote using the provided token
+        git config --global user.name 'github-actions[bot]'
+        git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+    fi
+
     if [[ ${FORCE_MOVE_RELEASE_TAG:-"NO"} == "YES" ]]; then
         move_tag
+
+        if [[ ${GITHUB_TOKEN+x} ]]; then
+            # We're running on GitHub Actions, so push the moved tag to the remote
+            git push origin :refs/tags/$latest_git_tag
+            git push
+            git push --tags
+            echo "Tag successfully moved on the remote server."
+        fi
     else 
         read -p "Do you want to commit changes to Package.swift and force move tag '$latest_git_tag'? (required for SPM release) [Y/n]" -n 1 -r
             echo    # (optional) move to a new line
