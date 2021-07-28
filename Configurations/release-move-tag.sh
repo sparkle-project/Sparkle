41f1f740086767aec128a96a9e3d0343b3c20328
Tony Arnold <tony@thecocoabots.com>
2021-07-28 20:28:35 +1000

    Restore original logic in release-move-tag
    It won't be used when publishing a release, so it can very nearly be removed entirely

--- Configurations/release-move-tag.sh
+++ Configurations/release-move-tag.sh
@@ -15,38 +15,17 @@ function move_tag() {
     echo "Package.swift committed and tag '$latest_git_tag' moved."
 }
 
-# if [ "$commits_since_tag" -gt 0 ]; then
-#     # If there have been commits since the latest tag, it's highly likely that we did not intend to do a full release
-#     echo "WARNING: $commits_since_tag commit(s) since tag '$latest_git_tag'. Did you tag a new version?"
-#     echo "Package.swift has not been committed and tag has not been moved."
-# else
-    # TODO: add sanity check to see if version is actually being updated or not?
-
-    if [[ ${GITHUB_TOKEN+x} ]]; then 
-        # We're running on GitHub Actions, so setup the actions username and a remote using the provided token
-        git config --global user.name 'github-actions[bot]'
-        git config --global user.email 'github-actions[bot]@users.noreply.github.com'
-    fi
-
-    if [[ ${FORCE_MOVE_RELEASE_TAG:-"NO"} == "YES" ]]; then
+if [ "$commits_since_tag" -gt 0 ]; then
+    # If there have been commits since the latest tag, it's highly likely that we did not intend to do a full release
+    echo "WARNING: $commits_since_tag commit(s) since tag '$latest_git_tag'. Did you tag a new version?"
+    echo "Package.swift has not been committed and tag has not been moved."
+else
+# TODO: add sanity check to see if version is actually being updated or not?
+    read -p "Do you want to commit changes to Package.swift and force move tag '$latest_git_tag'? (required for SPM release) [Y/n]" -n 1 -r
+        echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
         move_tag
-
-        if [[ ${GITHUB_TOKEN+x} ]]; then
-            # We're running on GitHub Actions, so push the moved tag to the remote
-            git push origin :refs/tags/$latest_git_tag
-            git push
-            git push origin $latest_git_tag
-            echo "Tag successfully moved on the remote server."
-        fi
-    else 
-        read -p "Do you want to commit changes to Package.swift and force move tag '$latest_git_tag'? (required for SPM release) [Y/n]" -n 1 -r
-            echo    # (optional) move to a new line
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            move_tag
-        else
-            echo "Package.swift has not been committed and tag has not been moved."
-        fi
-
+    else
+        echo "Package.swift has not been committed and tag has not been moved."
     fi
-
-# fi
+fi
