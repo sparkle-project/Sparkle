402a12b690c94a5ea75db016d7db1676c11c97c1
Zorg <zorgiepoo@gmail.com>
2021-08-05 01:28:00 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 // Version is technically not required here, SPM doesn't check
 let version = "2.0.0"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.0.0"
-let checksum = "ef0d0cc46421f8644f455b62537f4e4db2c97bdca5820641219a1836f3782878"
+let tag = "2.0.0-beta.1"
+let checksum = "f9b34f91a2dee4bf265640a793c445668c416f22019cbe7d8b9e0b31c838ab71"
 let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
