ea121b145810dd5bd0f2e023cdee790f2fb498aa
Zorg <zorgiepoo@gmail.com>
2021-08-15 17:48:42 -0700

    Update documentation for enums

--- Sparkle/SPUUserUpdateState.h
+++ Sparkle/SPUUserUpdateState.h
@@ -22,23 +22,59 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ A choice made by the user when prompted with a new update.
+ */
 typedef NS_ENUM(NSInteger, SPUUserUpdateChoice) {
+    /**
+     Dismisses the update and skips being notified of it in the future.
+     */
     SPUUserUpdateChoiceSkip,
+    /**
+     Downloads (if needed) and installs the update.
+     */
     SPUUserUpdateChoiceInstall,
+    /**
+     Dismisses the update until Sparkle reminds the user of it at a later time.
+     */
     SPUUserUpdateChoiceDismiss,
 };
 
+/**
+ Describes the current stage an update is undergoing.
+ */
 typedef NS_ENUM(NSInteger, SPUUserUpdateStage) {
+    /**
+     The update has not been downloaded.
+     */
     SPUUserUpdateStageNotDownloaded,
+    /**
+     The update has already been downloaded but not begun installing.
+     */
     SPUUserUpdateStageDownloaded,
+    /**
+     The update has already been downloaded and began installing in the background.
+     */
     SPUUserUpdateStageInstalling
 };
 
+/**
+ This represents the user's current update state.
+ */
 SU_EXPORT @interface SPUUserUpdateState : NSObject
 
 - (instancetype)init NS_UNAVAILABLE;
 
+/**
+ The current update stage.
+ 
+ This stage indicates if data has been already downloaded or not, or if an update is currently being installed.
+ */
 @property (nonatomic, readonly) SPUUserUpdateStage stage;
+
+/**
+ Indicates whether or not the update check was initiated by the user.
+ */
 @property (nonatomic, readonly) BOOL userInitiated;
 
 @end
