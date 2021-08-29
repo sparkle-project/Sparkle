515b8259bcd0defb08c28cdee8b697b045ffdeeb
runner <runner@Mac-1630198062773.local>
2021-08-29 03:37:49 +0000

    Update Package.swift and Sparkle.podspec for version 2.0.0-beta.1

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "2.0.0"
+let version = "2.0.0-beta.1"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.0.0"
-let checksum = "ef0d0cc46421f8644f455b62537f4e4db2c97bdca5820641219a1836f3782878"
+let tag = "2.0.0-beta.1"
+let checksum = "389a58bc0b333d9d18c8fa2d72aacbf4905d5d59a9858ecd9a5b16ff769bb5c5"
 let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
