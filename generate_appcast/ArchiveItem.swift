f115d881f2f26fe9ef02e5f4365c2ac1759a2de7
Zorg <zorgiepoo@gmail.com>
2021-10-29 02:13:00 -0700

    Fix generate_appcast compile error

--- generate_appcast/ArchiveItem.swift
+++ generate_appcast/ArchiveItem.swift
@@ -27,7 +27,7 @@ class DeltaUpdate {
     class func create(from: ArchiveItem, to: ArchiveItem, archivePath: URL) throws -> DeltaUpdate {
         var applyDiffError: NSError?
 
-        if !createBinaryDelta(from.appPath.path, to.appPath.path, archivePath.path, .beigeMajorVersion, false, &applyDiffError) {
+        if !createBinaryDelta(from.appPath.path, to.appPath.path, archivePath.path, .version2, false, &applyDiffError) {
             throw applyDiffError!
         }
 
