47f335614a1d2226b2b11e3f1f3991f9b0007236
Zorg <zorgiepoo@gmail.com>
2022-08-19 10:40:25 -0700

    Wait for a threshold amount of time before replying with status info data (#2230)
    This also reverts 6a69f26dcc58330f38a108d31efc81ea1372c8d5 and 4cb6719c6b3e9ddbb089840e7fb342d840208727

--- Autoupdate/StatusInfo.m
+++ Autoupdate/StatusInfo.m
@@ -12,15 +12,18 @@
 
 #include "AppKitPrevention.h"
 
+#define REPLY_STATUS_INFO_TIMEOUT 2
+
 @interface StatusInfo () <NSXPCListenerDelegate>
-
-@property (nonatomic) NSXPCListener *xpcListener;
-
 @end
 
 @implementation StatusInfo
+{
+    NSXPCListener *_xpcListener;
+    NSMutableDictionary *_pendingReplies;
+    NSUInteger _pendingReplyCounter;
+}
 
-@synthesize xpcListener = _xpcListener;
 @synthesize installationInfoData = _installationInfoData;
 
 - (instancetype)initWithHostBundleIdentifier:(NSString *)bundleIdentifier
@@ -29,19 +32,20 @@
     if (self != nil) {
         _xpcListener = [[NSXPCListener alloc] initWithMachServiceName:SPUStatusInfoServiceNameForBundleIdentifier(bundleIdentifier)];
         _xpcListener.delegate = self;
+        _pendingReplies = [NSMutableDictionary dictionary];
     }
     return self;
 }
 
 - (void)startListener
 {
-    [self.xpcListener resume];
+    [_xpcListener resume];
 }
 
 - (void)invalidate
 {
-    [self.xpcListener invalidate];
-    self.xpcListener = nil;
+    [_xpcListener invalidate];
+    _xpcListener = nil;
 }
 
 - (BOOL)listener:(NSXPCListener *)__unused listener shouldAcceptNewConnection:(NSXPCConnection *)newConnection
@@ -54,10 +58,41 @@
     return YES;
 }
 
+- (void)setInstallationInfoData:(NSData *)installationInfoData
+{
+    _installationInfoData = installationInfoData;
+    
+    // Respond to all of our pending replies
+    for (NSNumber *replyKey in _pendingReplies) {
+        void (^replyBlock)(NSData * _Nullable) = self->_pendingReplies[replyKey];
+        replyBlock(_installationInfoData);
+    }
+    
+    [_pendingReplies removeAllObjects];
+}
+
 - (void)probeStatusInfoWithReply:(void (^)(NSData * _Nullable))reply
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        reply(self.installationInfoData);
+        if (self->_installationInfoData != nil) {
+            reply(self->_installationInfoData);
+        } else {
+            // If we don't have the installation info data currently, we may receive it in a very short window afterwards
+            // In this case wait a bit for the reply. If we receive the data it will be in -setInstallationInfoData:
+            NSUInteger currentReplyCounter = self->_pendingReplyCounter;
+            self->_pendingReplyCounter++;
+
+            NSNumber *currentReplyCounterKey = @(currentReplyCounter);
+            self->_pendingReplies[currentReplyCounterKey] = [reply copy];
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(REPLY_STATUS_INFO_TIMEOUT * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                void (^replyBlock)(NSData * _Nullable) = self->_pendingReplies[currentReplyCounterKey];
+                if (replyBlock != nil) {
+                    replyBlock(self->_installationInfoData);
+                    [self->_pendingReplies removeObjectForKey:currentReplyCounterKey];
+                }
+            });
+        }
     });
 }
 
