0bdd36a1bc2472d2f4040317476e7e4f5271a53a
Zorg <zorgiepoo@gmail.com>
2022-08-21 20:28:18 -0700

    Ensure install handlers are invoked on main queue

--- Sparkle/SPUAutomaticUpdateDriver.m
+++ Sparkle/SPUAutomaticUpdateDriver.m
@@ -110,7 +110,9 @@
         if (self.willInstallSilently && [updaterDelegate respondsToSelector:@selector(updater:willInstallUpdateOnQuit:immediateInstallationBlock:)]) {
             __weak SPUAutomaticUpdateDriver *weakSelf = self;
             installationHandledByDelegate = [updaterDelegate updater:self.updater willInstallUpdateOnQuit:self.updateItem immediateInstallationBlock:^{
-                [weakSelf.coreDriver finishInstallationWithResponse:SPUUserUpdateChoiceInstall displayingUserInterface:NO];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [weakSelf.coreDriver finishInstallationWithResponse:SPUUserUpdateChoiceInstall displayingUserInterface:NO];
+                });
             }];
         }
         
