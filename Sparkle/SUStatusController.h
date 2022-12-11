4d6df9f37265200a7012b9d123c27f9889615bf7
Zorg <zorgiepoo@gmail.com>
2022-12-10 22:08:14 -0800

    Fix direct annotations for SUStatusController.h

--- Sparkle/SUStatusController.h
+++ Sparkle/SUStatusController.h
@@ -14,21 +14,23 @@
 #import <Cocoa/Cocoa.h>
 
 @class SUHost;
-__attribute__((objc_direct_members)) @interface SUStatusController : NSWindowController
+@interface SUStatusController : NSWindowController
 
-@property (nonatomic, copy, direct) NSString *statusText;
-@property (nonatomic, direct) double progressValue;
-@property (nonatomic, direct) double maxProgressValue;
+// These three properties are connected via bindings
+@property (nonatomic, copy) NSString *statusText;
+@property (nonatomic) double progressValue;
+@property (nonatomic) double maxProgressValue;
+
 @property (nonatomic, getter=isButtonEnabled, direct) BOOL buttonEnabled;
 
-- (instancetype)initWithHost:(SUHost *)aHost windowTitle:(NSString *)windowTitle centerPointValue:(NSValue *)centerPointValue minimizable:(BOOL)minimizable closable:(BOOL)closable;
+- (instancetype)initWithHost:(SUHost *)aHost windowTitle:(NSString *)windowTitle centerPointValue:(NSValue *)centerPointValue minimizable:(BOOL)minimizable closable:(BOOL)closable __attribute__((objc_direct));
 
 // Pass 0 for the max progress value to get an indeterminate progress bar.
 // Pass nil for the status text to not show it.
-- (void)beginActionWithTitle:(NSString *)title maxProgressValue:(double)maxProgressValue statusText:(NSString *)statusText;
+- (void)beginActionWithTitle:(NSString *)title maxProgressValue:(double)maxProgressValue statusText:(NSString *)statusText __attribute__((objc_direct));
 
 // If isDefault is YES, the button's key equivalent will be \r.
-- (void)setButtonTitle:(NSString *)buttonTitle target:(id)target action:(SEL)action isDefault:(BOOL)isDefault;
+- (void)setButtonTitle:(NSString *)buttonTitle target:(id)target action:(SEL)action isDefault:(BOOL)isDefault __attribute__((objc_direct));
 
 @end
 
