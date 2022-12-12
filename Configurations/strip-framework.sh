536fbfb97272e4bd3e4eccfaf26c8f1d1c41beb3
Zorg <zorgiepoo@gmail.com>
2022-12-11 22:30:43 -0800

    Add flag to not copy localization files

--- Configurations/strip-framework.sh
+++ Configurations/strip-framework.sh
@@ -39,3 +39,20 @@ if [[ "$SPARKLE_BUILD_UI_BITS" -eq 0 ]]; then
     rm -rf "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/Resources"/"Base.lproj/SUUpdatePermissionPrompt.nib"
     rm -rf "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/Resources"/"ReleaseNotesColorStyle.css"
 fi
+
+# Remove localization files if requested
+
+if [[ "$SPARKLE_COPY_LOCALIZATIONS" -eq 0 ]]; then
+    for dir in "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/Resources"/*; do
+        base=$(basename "$dir")
+        if [[ "$base" =~ .*".lproj" ]]; then
+            if [[ "$base" = "Base.lproj" ]]; then
+                rm -rf "$dir/Sparkle.strings"
+                # Remove Base.lproj if it's empty and the nibs have been stripped out already
+                rmdir "$dir"
+            else
+                rm -rf "$dir"
+            fi
+        fi
+    done
+fi
