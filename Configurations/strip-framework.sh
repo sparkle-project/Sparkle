d58fb1a374a6e5f867f284fc9d5d5ede13afdcde
Kent Sutherland <git@ksuther.com>
2023-04-02 13:42:01 -0400

    Conditionally copy XPC services using copy-xpc-services.sh Before we were copying all XPC services, then conditionally deleting them This would cause Sparkle to appear dirty to downstream dependencies and would slow down incremental builds because they'd have to copy and sign Sparkle every time

--- Configurations/strip-framework.sh
+++ Configurations/strip-framework.sh
@@ -2,35 +2,6 @@
 
 FRAMEWORK_PATH="${TARGET_BUILD_DIR}"/"${FULL_PRODUCT_NAME}"
 
-# Remove any unused XPC Services
-
-removedservices=0
-
-if [[ "$SPARKLE_EMBED_INSTALLER_LAUNCHER_XPC_SERVICE" -eq 0 ]]; then
-    rm -rf "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/XPCServices"/"${INSTALLER_LAUNCHER_NAME}.xpc"
-    removedservices=$((removedservices+1))
-fi
-
-if [[ "$SPARKLE_EMBED_DOWNLOADER_XPC_SERVICE" -eq 0 ]]; then
-    rm -rf "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/XPCServices"/"${DOWNLOADER_NAME}.xpc"
-    removedservices=$((removedservices+1))
-fi
-
-if [[ "$SPARKLE_EMBED_INSTALLER_STATUS_XPC_SERVICE" -eq 0 ]]; then
-    rm -rf "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/XPCServices"/"${INSTALLER_STATUS_NAME}.xpc"
-    removedservices=$((removedservices+1))
-fi
-
-if [[ "$SPARKLE_EMBED_INSTALLER_CONNECTION_XPC_SERVICE" -eq 0 ]]; then
-    rm -rf "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/XPCServices"/"${INSTALLER_CONNECTION_NAME}.xpc"
-    removedservices=$((removedservices+1))
-fi
-
-if [[ "$removedservices" -eq 4 ]]; then
-    rm -rf "${FRAMEWORK_PATH}"/"Versions/"${FRAMEWORK_VERSION}"/XPCServices"
-    rm -rf "${FRAMEWORK_PATH}"/"XPCServices"
-fi
-
 # Remove any unused nibs
 
 if [[ "$SPARKLE_BUILD_UI_BITS" -eq 0 ]]; then
