40c30bea5b7c695955b6d34c7113e749ba80120f
Zorg <zorgiepoo@gmail.com>
2023-10-16 20:44:29 -0700

    Revert "Update Package management files for version 2.5.2-beta.1"
    This reverts commit 8a5ea42a2db4904776ae75559c11946c0096f0f6.

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "2.5.2-beta.1"
+let version = "2.5.1"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.5.2-beta.1"
-let checksum = "e0eecd69913c496fc0afe1f584b3509073c99b82db54c157bb91f20a53a053f9"
+let tag = "2.5.1"
+let checksum = "c8948eecbcd0c51ffdf78b6a12283851b904f038f73a45b9088f82a3d0d83c41"
 let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
