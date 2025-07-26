5109b17b654604c5497d097f6ae8dfc3ece53c44
Zorg <zorgiepoo@gmail.com>
2025-07-26 15:12:55 -0700

    Skip workaround for macOS versions less than 26
    Also perform IO directory check as last part of the workaround.

--- Sparkle/InstallerProgress/InstallerProgressAppController.m
+++ Sparkle/InstallerProgress/InstallerProgressAppController.m
@@ -231,22 +231,37 @@ static const NSTimeInterval SUTerminationTimeDelay = 0.3;
         // We will want to terminate those apps and observe their pids, but we will only do this if we don't find any regular matches
         NSMutableArray<NSRunningApplication *> *potentialMatchingTranslocatedRunningApplications = [[NSMutableArray alloc] init];
         
+        BOOL needsToHandleImproperBundles;
+        if (@available(macOS 16, *)) {
+            needsToHandleImproperBundles = NO;
+        } else {
+            needsToHandleImproperBundles = YES;
+        }
+        
         for (NSRunningApplication *runningApplication in runningApplications) {
             // Comparing the URLs hasn't worked well for me in practice, so I'm comparing the file paths instead
             NSString *candidatePath = runningApplication.bundleURL.URLByResolvingSymlinksInPath.path;
             if (candidatePath != nil) {
                 NSArray<NSString *> *candidatePathComponents = candidatePath.pathComponents;
-                NSUInteger candidatePathComponentsCount = candidatePathComponents.count;
 
-                // Workaround cases where macOS appends Contents/MacOS/<executable> to the bundle path which can happen in corner cases (eg: mishandled bundles or helper executables).
-                // https://github.com/sparkle-project/Sparkle/issues/2725
-                BOOL candidatePathIsDir = YES;
-                if ([fileManager fileExistsAtPath:candidatePath isDirectory:&candidatePathIsDir] && !candidatePathIsDir) {
+                if (needsToHandleImproperBundles) {
+                    // Workaround cases where macOS appends Contents/MacOS/<executable> to the bundle path which can happen in corner cases (eg: mishandled bundles or helper executables).
+                    // This may happen with app bundles which have wrapper/tramopline executables that spawn the main executable
+                    // https://github.com/sparkle-project/Sparkle/issues/2725
+                    
+                    NSUInteger candidatePathComponentsCount = candidatePathComponents.count;
+                    // > 3 is fine instead of >= 3 because there should be another parent path component for the bundle
                     if (candidatePathComponentsCount > 3 && [candidatePathComponents[candidatePathComponentsCount - 3] isEqualToString:@"Contents"] && [candidatePathComponents[candidatePathComponentsCount - 2] isEqualToString:@"MacOS"]) {
-                        NSMutableArray<NSString *> *trimmedBundlePath = [candidatePathComponents mutableCopy];
-                        [trimmedBundlePath removeObjectsInRange:NSMakeRange(candidatePathComponentsCount - 3, 3)];
-                        candidatePathComponents = trimmedBundlePath;
-                        candidatePathComponentsCount -= 3;
+                        
+                        // Lastly verify if the bundle is not actually a directory
+                        // Note the IO check is the last check for this corner case
+                        // This ensures it's not an ordinary helper app that happens to be inside another app bundle.
+                        BOOL candidatePathIsDir = YES;
+                        if ([fileManager fileExistsAtPath:candidatePath isDirectory:&candidatePathIsDir] && !candidatePathIsDir) {
+                            NSMutableArray<NSString *> *trimmedBundlePath = [candidatePathComponents mutableCopy];
+                            [trimmedBundlePath removeObjectsInRange:NSMakeRange(candidatePathComponentsCount - 3, 3)];
+                            candidatePathComponents = trimmedBundlePath;
+                        }
                     }
                 }
 
