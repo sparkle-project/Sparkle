bbefcdf94c469a35d8495707f841b8fa196801a2
TranPhuong319 <duyphuong3a2@gmail.com>
2025-12-24 23:56:42 +0700

    Add Vietnamese language

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let version = "2.8.1"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
 let tag = "2.8.1"
-let checksum = "995f913935fe903d50b3be5344fc52a638f56b39ef7984bf7a44244786d28638"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let checksum = "3493cd9e9cef07ecf5e35036f00d5024b17919c17d09fb5cffd4a70e7f261a88"
+let url = "https://github.com/TranPhuong319/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
