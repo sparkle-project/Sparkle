c34faaed4cd4156653aea8aac23fe0b6bd497257
Zorg <zorgiepoo@gmail.com>
2026-08-15 06:27:54 -0700

    Don't copy progress tool for root user (#2897)
    This way we don't have to create cache directories under a different user to launch the progress tool. Longer term I want to never copy the tool.

--- InstallerLauncher/SUInstallerLauncher.m
+++ InstallerLauncher/SUInstallerLauncher.m
@@ -32,7 +32,7 @@
 
 @implementation SUInstallerLauncher
 
-- (BOOL)submitProgressToolAtPath:(NSString *)progressToolPath withHostBundle:(NSBundle *)hostBundle inSystemDomainForInstaller:(BOOL)inSystemDomainForInstaller SPU_OBJC_DIRECT
+- (BOOL)submitProgressToolAtPath:(NSString *)progressToolPath withHostBundle:(NSBundle *)hostBundle inSystemDomainForInstaller:(BOOL)inSystemDomainForInstaller copiedApplication:(BOOL)copiedApplication SPU_OBJC_DIRECT
 {
     SUFileManager *fileManager = [[SUFileManager alloc] init];
     
@@ -53,7 +53,7 @@
     NSString *hostBundleIdentifier = hostBundle.bundleIdentifier;
     assert(hostBundleIdentifier != nil);
     
-    NSArray<NSString *> *arguments = @[executablePath, hostBundlePath, @(inSystemDomainForInstaller).stringValue];
+    NSArray<NSString *> *arguments = @[executablePath, hostBundlePath, @(inSystemDomainForInstaller).stringValue, @(copiedApplication).stringValue];
     
     // The progress tool can only be ran as the logged in user, not as root
     CFStringRef domain = kSMDomainUserLaunchd;
@@ -506,8 +506,6 @@ static BOOL SPUUsesSystemDomainForBundlePath(NSString *path, BOOL rootUser
         
         NSString *userName;
         NSString *homeDirectory;
-        uid_t uid = 0;
-        gid_t gid = 0;
         if (!rootUser) {
             // Normal path
             homeDirectory = NSHomeDirectory();
@@ -518,7 +516,7 @@ static BOOL SPUUsesSystemDomainForBundlePath(NSString *path, BOOL rootUser
         } else {
             // As the root user we need to obtain the user name and home directory reflecting
             // the user's console session.
-            CFStringRef userNameRef = SCDynamicStoreCopyConsoleUser(NULL, &uid, &gid);
+            CFStringRef userNameRef = SCDynamicStoreCopyConsoleUser(NULL, NULL, NULL);
             if (userNameRef == NULL) {
                 SULog(SULogLevelError, @"Failed to retrieve user name from the console user");
                 completionHandler(SUInstallerLauncherFailure, inSystemDomain);
@@ -539,55 +537,48 @@ static BOOL SPUUsesSystemDomainForBundlePath(NSString *path, BOOL rootUser
         // We can't compare the signature of this framework/XPC service (depending how it's run) to the host bundle because
         // they could be different (eg: take a look at sparkle-cli). We also can't easily tell if the signature of the service/framework is the same as the bundle it's inside.
         // The service/framework also need not even be signed in the first place. We'll just assume for now the original bundle hasn't been tampered with
-        NSString *cachePath = rootUser ?
-            [SPULocalCacheDirectory cachePathForBundleIdentifier:hostBundleIdentifier userName:userName] :
-            [SPULocalCacheDirectory cachePathForBundleIdentifier:hostBundleIdentifier];
         
-        NSString *rootLauncherCachePath = [cachePath stringByAppendingPathComponent:@"Launcher"];
-        
-        [SPULocalCacheDirectory removeOldItemsInDirectory:rootLauncherCachePath];
-        
-        NSDictionary<NSFileAttributeKey, id> *fileAttributes = rootUser ?
-            @{NSFileOwnerAccountID: @(uid), NSFileGroupOwnerAccountID: @(gid)} :
-            nil;
-        
-        NSString *launcherCachePath = [SPULocalCacheDirectory createUniqueDirectoryInDirectory:rootLauncherCachePath intermediateDirectoryFileAttributes:fileAttributes];
-        
-        if (launcherCachePath == nil) {
-            SULog(SULogLevelError, @"Failed to create cache directory for progress tool in %@", rootLauncherCachePath);
-            completionHandler(SUInstallerLauncherFailure, inSystemDomain);
-            return;
-        }
-        
-        SUFileManager *fileManager = [[SUFileManager alloc] init];
-        
-        if (rootUser) {
-            // Ensure the console user has ownership of the launcher cache directory
-            // Otherwise the updater may not launch and not be able to clean up itself
-            NSError *changeOwnerAndGroupError = nil;
-            if (![fileManager changeOwnerAndGroupOfItemAtURL:[NSURL fileURLWithPath:launcherCachePath] ownerID:uid groupID:gid error:&changeOwnerAndGroupError]) {
-                SULog(SULogLevelError, @"Failed to change owner and group for launcher cache directory: %@", changeOwnerAndGroupError);
-                
+        // Don't create cache directories or copy the progress tool app outside of the bundle for root user,
+        // because the app will not start if it's contained inside directories owned by root,
+        // and un-doing that opens up potential security issues.
+        // Eventually we will also want to not copy the tool app in the !rootUser case, minimizing risk
+        // by not making that change yet.
+        BOOL copyProgressTool = !rootUser;
+        NSString *progressToolPath;
+        if (copyProgressTool) {
+            NSString *cachePath = [SPULocalCacheDirectory cachePathForBundleIdentifier:hostBundleIdentifier];
+            
+            NSString *rootLauncherCachePath = [cachePath stringByAppendingPathComponent:@"Launcher"];
+            
+            [SPULocalCacheDirectory removeOldItemsInDirectory:rootLauncherCachePath];
+            
+            NSString *launcherCachePath = [SPULocalCacheDirectory createUniqueDirectoryInDirectory:rootLauncherCachePath];
+            
+            if (launcherCachePath == nil) {
+                SULog(SULogLevelError, @"Failed to create cache directory for progress tool in %@", rootLauncherCachePath);
                 completionHandler(SUInstallerLauncherFailure, inSystemDomain);
                 return;
             }
-        }
-        
-        NSString *progressToolPath = [launcherCachePath stringByAppendingPathComponent:@""SPARKLE_INSTALLER_PROGRESS_TOOL_NAME@".app"];
-        
-        NSError *copyError = nil;
-        // SUFileManager is more reliable for copying files around
-        if (![fileManager copyItemAtURL:[NSURL fileURLWithPath:progressToolResourcePath] toURL:[NSURL fileURLWithPath:progressToolPath] error:&copyError]) {
-            SULog(SULogLevelError, @"Failed to copy progress tool to cache: %@", copyError);
-            completionHandler(SUInstallerLauncherFailure, inSystemDomain);
-            return;
+            
+            progressToolPath = [launcherCachePath stringByAppendingPathComponent:@""SPARKLE_INSTALLER_PROGRESS_TOOL_NAME@".app"];
+            
+            NSError *copyError = nil;
+            // SUFileManager is more reliable for copying files around
+            SUFileManager *fileManager = [[SUFileManager alloc] init];
+            if (![fileManager copyItemAtURL:[NSURL fileURLWithPath:progressToolResourcePath] toURL:[NSURL fileURLWithPath:progressToolPath] error:&copyError]) {
+                SULog(SULogLevelError, @"Failed to copy progress tool to cache: %@", copyError);
+                completionHandler(SUInstallerLauncherFailure, inSystemDomain);
+                return;
+            }
+        } else {
+            progressToolPath = progressToolResourcePath;
         }
         
         SUInstallerLauncherStatus installerStatus = [self submitInstallerAtPath:installerPath withHostBundle:hostBundle iconBundlePath:mainBundle.bundlePath updaterIdentifier:updaterIdentifier userName:userName homeDirectory:homeDirectory mainBundleName:mainBundleName inSystemDomain:inSystemDomain rootUser:rootUser];
         
         BOOL submittedProgressTool = NO;
         if (installerStatus == SUInstallerLauncherSuccess) {
-            submittedProgressTool = [self submitProgressToolAtPath:progressToolPath withHostBundle:hostBundle inSystemDomainForInstaller:inSystemDomain];
+            submittedProgressTool = [self submitProgressToolAtPath:progressToolPath withHostBundle:hostBundle inSystemDomainForInstaller:inSystemDomain copiedApplication:copyProgressTool];
             
             if (!submittedProgressTool) {
                 SULog(SULogLevelError, @"Failed to submit progress tool job");
